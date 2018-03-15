94b1388869ff752f37f4b9539f68ab98d0e68ad4
Thomas <thomas@infoprojects.nl>
2018-03-15 12:15:23 +0100

    #32-formulieren-redesign

---
+++ .fractal/collator.js
@@ -0,0 +1,18 @@
+module.exports = (markup, item) =>
+  item.context.__title || item.context.__explanation
+    ? `
+      <!-- Start: @${item.handle} -->
+      <div class="dso-example-wrapper">
+        <div class="dso-example-text">
+          ${item.context.__title ? `<h2>${item.context.__title}</h2>` : ''}
+          ${item.context.__explanation
+      ? [].concat(item.context.__explanation).map(e => `<p>${e}</p>`).join('\n')
+      : ''}
+          </div>
+          <div class="dso-example">
+            ${markup}
+          </div>
+        </div>
+        <hr />
+        <!-- End: @${item.handle} -->`
+    : `${markup}<hr />`;
