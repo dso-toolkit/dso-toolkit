af7460eeb16c2d35a9d5c7d5f42ca9af18a2b427
Dion Verhelst <dion.verhelst@infoprojects.nl>
2018-06-08 16:46:57 +0200

    #71 Searchbar component

--- .fractal/collator.js
+++ .fractal/collator.js
@@ -1,14 +1,19 @@
 module.exports = (markup, item) =>
   `<!-- Start: @${item.handle} -->\n` +
-  (item.context.__title || item.context.__explanation
+  (item.context.__title || item.context.__explanation || item.context.__switches
     ? `
       <div class="dso-example-wrapper">
-        <div class="dso-example-text" aria-hidden="true">
-          ${item.context.__title ? `<h2>${item.context.__title}</h2>` : ''}
-          ${item.context.__explanation
-            ? [].concat(item.context.__explanation).map(e => `<p>${e}</p>`).join('\n')
-            : ''}
-        </div>
+        ${item.context.__title || item.context.__explanation
+          ? `<div class="dso-example-text" aria-hidden="true">
+              ${item.context.__title ? `<h2>${item.context.__title}</h2>` : ''}
+              ${item.context.__explanation
+                ? [].concat(item.context.__explanation).map(e => `<p>${e}</p>`).join('\n')
+                : ''}
+            </div>`
+          : ''}
+        ${item.context.__switches && item.context.__switches.length > 0
+          ? item.context.__switches.map(s => `<input type="checkbox" id="${s.name}-switch" /><label for="${s.name}-switch">${s.label}</label>`)
+          : ''}
         <div class="dso-example">
           ${markup}
         </div>
