7d276ee1d27b7d6cbdfd03cf674f4694fe7c86c2
Thomas <thomas@infoprojects.nl>
2018-08-01 17:59:14 +0200

    #103-selected-filter-component

--- components/04-indicators/badge/badge.config.js
+++ components/04-indicators/badge/badge.config.js
@@ -1,22 +1,11 @@
 const collator = require('../../../.fractal/collator');
-console.log(collator);
-console.log(collator.defaultCollator);
-console.log(collator.inlineCollator);
-module.exports = {
-  notes: `Plaats een badge. Een badge kan verschillende verschijningsvormen hebben, waarbij men op het <span> element naast de class '.dso-badge' een extra class zet.
 
-    Default: '.dso-badge .badge-default'
-    Info: '.dso-badge .badge-info'
-    Primary: '.dso-badge .badge-primary'
-    Success: '.dso-badge .badge-success'
-    Warning: '.dso-badge .badge-warning'
-    Error: '.dso-badge .badge-danger'`,
+module.exports = {
   status: 'wip',
   collated: true,
   collator: collator.inlineCollator,
   context: {
     label: 'Default',
-    modifier: 'default'
   },
   variants: [
     {
--- components/04-indicators/label/label.config.js
+++ components/04-indicators/label/label.config.js
@@ -1,25 +1,17 @@
 const collator = require('../../../.fractal/collator');
 
 module.exports = {
-  notes: `Plaats een label. Een label kan verschillende verschijningsvormen hebben, waarbij men op het <span> element naast de class '.dso-label' een extra class zet.
-
-    Default: '.dso-label .label-default'
-    Info: '.dso-label .label-info'
-    Primary: '.dso-label .label-primary'
-    Success: '.dso-label .label-success'
-    Warning: '.dso-label .label-warning'
-    Error: '.dso-label .label-danger'
-
-    Om een label met sluitknop te krijgen moet '.closable' worden toegevoegd op het niveau van '.label'.`,
   status: 'wip',
   collated: true,
   collator: collator.inlineCollator,
-  context: {
-    label: 'Default',
-    modifier: 'default',
-    buttonText: 'Sluiten'
-  },
+  default: 'default',
   variants: [
+    {
+      name: 'default',
+      context: {
+        label: 'Default',
+      }
+    },
     {
       name: 'label-info',
       context: {
@@ -54,13 +46,15 @@ module.exports = {
         label: 'Danger',
         modifier: 'danger'
       }
-    },
-    {
-      name: 'label-filter',
-      context: {
-        label: 'Filter label',
-        modifier: 'default has-button'
-      }
     }
-  ]
+  ].reduce((total, variant) => total.concat([
+    variant,
+    Object.assign({}, variant, {
+      name: `${variant.name}-button`,
+      context: Object.assign({}, variant.context, {
+        button: 'fa fa-times',
+        buttonTitle: 'Verwijder'
+      })
+    })
+  ]), [])
 };
