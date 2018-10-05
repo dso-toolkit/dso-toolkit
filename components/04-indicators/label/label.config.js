584ad0b87cdbc63837854d57a7a9abb529610970
Pieter Verheij <pieter.verheij@infoprojects.nl>
2018-10-05 16:04:40 +0200

    #194.Label-spacing-padding-too-small : removed extra tag, changed floating to button

--- components/04-indicators/label/label.config.js
+++ components/04-indicators/label/label.config.js
@@ -3,7 +3,7 @@ const collator = require('../../../.fractal/collator');
 module.exports = {
   status: 'wip',
   collated: true,
-  collator: (markup, item) => `<div class="col-xs-6${item.order % 2 === 1 ? ' text-right ' : ''}" ${item.order % 2 === 1 ? 'style=\"clear: left;\"' : ''}>${markup}</div>`,
+  collator: (markup, item) => `<div class="col-xs-6${item.order % 2 === 1 ? ' text-right ' : ''}">${markup}<br /><br /></div>`,
   default: 'default',
   variants: [
     {
