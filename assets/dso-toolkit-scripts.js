014c87661c460242129caa49ae99ce5a377b342a
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2019-06-17 17:31:15 +0200

    #322 Documentatie: Startgids en toelichting Modal component

--- assets/dso-toolkit-scripts.js
+++ assets/dso-toolkit-scripts.js
@@ -1,6 +1,4 @@
 function DsoToolkitResizeIframe(frame, element) {
-  console.log(frame, element);
-
   const height = typeof element === 'string'
     ? frame.contentWindow.document.querySelector(element).scrollHeight
     : frame.contentWindow.document.body.scrollHeight;
