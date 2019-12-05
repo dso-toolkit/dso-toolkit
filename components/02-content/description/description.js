61e3fbca10bbb474e3dd51865719874a80c21761
Pieter Verheij <pieter.verheij@infoprojects.nl>
2019-12-05 17:29:50 +0100

    aria-collapsed -> aria-expandend

--- components/02-content/description/description.js
+++ components/02-content/description/description.js
@@ -7,10 +7,10 @@ module.exports = function () {
   return {
     dsoDescription(term, options) {
       return [
-        `<a id="${encodeURI(term)}-term" href="#${encodeURI(term)}-content" class="dso-description-term${this['description-hidden'] ? '' : ' dso-is-open'}" aria-collapsed="${!!this['description-hidden']}" aria-controls="${encodeURI(term)}-content">${term}</a>`,
+        `<a id="${encodeURI(term)}-term" href="#${encodeURI(term)}-content" class="dso-description-term${this['description-hidden'] ? '' : ' dso-is-open'}" aria-expandend="${this['description-hidden'] ? 'false' : 'true'}" aria-controls="${encodeURI(term)}-content">${term}</a>`,
         `<span id="${encodeURI(term)}-content" class="dso-description-content ${this['description-hidden'] ? ' dso-description-collapsed' : ' dso-description-expanded'}">`,
           `<a href="#${encodeURI(term)}-term">`,
-            `<span class="sr-only">Sluiten</span>`,
+            `<span class="sr-only">Verbergen</span>`,
           `</a>`,
           `<span>${options.fn(this)}</span>`,
         `</span>`
