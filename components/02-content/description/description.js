7b7f39546c8739789064ded5248b88633a35e043
Pieter Verheij <pieter.verheij@infoprojects.nl>
2019-12-11 10:58:09 +0100

    typefoutje opgelost

--- components/02-content/description/description.js
+++ components/02-content/description/description.js
@@ -7,7 +7,7 @@ module.exports = function () {
   return {
     dsoDescription(term, options) {
       return [
-        `<a id="${encodeURI(term)}-term" href="#${encodeURI(term)}-content" class="dso-description-term${this['description-hidden'] ? '' : ' dso-is-open'}" aria-expandend="${this['description-hidden'] ? 'false' : 'true'}" aria-controls="${encodeURI(term)}-content">${term}</a>`,
+        `<a id="${encodeURI(term)}-term" href="#${encodeURI(term)}-content" class="dso-description-term${this['description-hidden'] ? '' : ' dso-is-open'}" aria-expanded="${this['description-hidden'] ? 'false' : 'true'}" aria-controls="${encodeURI(term)}-content">${term}</a>`,
         `<span id="${encodeURI(term)}-content" class="dso-description-content ${this['description-hidden'] ? ' dso-description-collapsed' : ' dso-description-expanded'}">`,
           `<a href="#${encodeURI(term)}-term">`,
             `<span class="sr-only">Verbergen</span>`,
