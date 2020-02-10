80d7c5e8132c396d3d1583eb44f6865fc6523a40
Pieter Verheij <pieter.verheij@infoprojects.nl>
2020-02-10 11:05:46 +0100

    wip

---
+++ components/02-content/accordion/light/accordion-light.js
@@ -0,0 +1,41 @@
+// Notes:
+// ------
+// * Each change to this file needs a restart of the fractal instance
+// * Exported methods are merged as helpers in fractal.js
+
+module.exports = function () {
+  return {
+    accordionLight(options) {
+      const classNames = ['dso-accordion-light', this.modifiers].filter(c => c).join(' ');
+
+      return `
+        <div class="${classNames}">
+          ${options.fn(this)}
+        </div>
+      `.trim();
+    },
+
+    accordionLightSection(title, options) {
+      const self = this;
+      const header = 'h3';
+
+      let html = (
+        `<${header} class="dso-section-handle${(self.state ? ' dso-' + self.state : '') + (self.open ? ' dso-open' : '') }">
+          <button>`);
+
+      html += title;
+
+      html += (`</button>\n</${header}>`);
+
+      if (self.open) {
+        html += (
+          `<div class="dso-section-body">
+            ${options.fn(self)}
+          </div>`
+        );
+      }
+
+      return html.trim();
+    }
+  };
+};
