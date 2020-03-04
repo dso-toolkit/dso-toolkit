32bf1d01f27de697bbb88d08be5f53ca5587151a
Pieter Verheij <pieter.verheij@infoprojects.nl>
2020-03-04 17:11:28 +0100

    #573 review opmerkingen verwerkt

--- nunjucks/filters.js
+++ nunjucks/filters.js
@@ -1,5 +1,13 @@
 module.exports = {
   split: function(input, separator) {
     return input.split(separator);
+  },
+  map: function(arr, property) {
+    return arr.map(function (item) {
+      return item[property];
+    });
+  },
+  includes: function(arr, value) {
+    return arr.includes(value);
   }
 };
