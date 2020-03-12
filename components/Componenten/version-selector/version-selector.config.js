10d861f594f0b0c77e0f933fcbf9fc1ca851a6b9
Pieter Verheij <pieter.verheij@infoprojects.nl>
2020-03-12 10:24:39 +0100

    #499 logo en version selector

---
+++ components/Componenten/version-selector/version-selector.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  notes: 'Toont een dropdown met daarin een gevulde uitklapper. Gebaseerd op de Bootstrap "Dropdowns" component',
+  status: 'ready',
+  collated: true,
+  collator: function (markup, item) {
+    return `<li>${markup}</li>`;
+  },
+  default: 'dropdown',
+  context: {
+    id: 'actie',
+    open: false,
+    label: 'Actie'
+  },
+  variants: [
+    {
+      name: 'dropdown-open',
+      context: {
+        id: 'actie_open',
+        open: true,
+        groups: [
+          {
+            items: [
+              {
+                label: 'Actie'
+              },
+              {
+                label: 'Andere actie'
+              },
+              {
+                label: 'Nog iets anders hier'
+              }
+            ]
+          },
+          {
+            items: [
+              {
+                label: 'Gescheiden link'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  ]
+};
