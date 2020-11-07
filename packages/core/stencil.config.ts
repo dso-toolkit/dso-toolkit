ee6431f0426ce9aa2973025b4f292d8ed9dfa613
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2020-11-07 18:00:46 +0100

    #844 Packages "@dso-toolkit/core" en "@dso-toolkit/styling" opstarten

---
+++ packages/core/stencil.config.ts
@@ -0,0 +1,25 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'dso-toolkit',
+  plugins: [
+    sass()
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
