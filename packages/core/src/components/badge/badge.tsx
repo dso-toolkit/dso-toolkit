e09080690091ff90464b9ad70769e0e14b18f89f
Dion Verhelst <dion.verhelst@infoprojects.nl>
2020-11-12 15:59:22 +0100

    #852 Web Component: Badge

---
+++ packages/core/src/components/badge/badge.tsx
@@ -0,0 +1,26 @@
+import { Component, h, Prop } from '@stencil/core';
+import clsx from 'clsx';
+
+@Component({
+  tag: 'dso-badge',
+  styleUrl: 'badge.scss',
+  shadow: true
+})
+export class Badge {
+  @Prop()
+  level?: 'primary' | 'success' | 'info' | 'warning' | 'danger';
+
+  @Prop()
+  status?: string;
+
+  render() {
+    return (
+      <span class={clsx('dso-badge', { [`badge-${this.level}`]: this.level })}>
+        {this.status && (
+          <span class="sr-only">{this.status}: </span>
+        )}
+        <slot></slot>
+      </span>
+    );
+  }
+}
