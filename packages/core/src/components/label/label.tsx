f56b8db0e4df9867485cd818da1359995e0efb76
PieterVerheij <pieter@infoprojects.nl>
2020-11-19 16:44:05 +0100

    #861 Web Component: Label

---
+++ packages/core/src/components/label/label.tsx
@@ -0,0 +1,33 @@
+import { Component, h, Prop } from '@stencil/core';
+import clsx from 'clsx';
+
+@Component({
+  tag: 'dso-label',
+  styleUrl: 'label.scss',
+  shadow: true
+})
+export class Label {
+  @Prop()
+  status?: 'primary' | 'info' | 'success' | 'warning' | 'danger';
+
+  private static statusMap = new Map<string, string>([
+    ['primary', 'Primair'],
+    ['info', 'Info'],
+    ['success', 'Succes'],
+    ['warning', 'Waarschuwing'],
+    ['danger', 'Gevaar']
+  ]);
+
+  render() {
+    const status = this.status && Label.statusMap.get(this.status);
+
+    return (
+      <span class={clsx('dso-label', { [`dso-label-${this.status}`]: this.status } )}>
+        {status && (
+          <span class="sr-only">{status}: </span>
+        )}
+        <slot></slot>
+      </span>
+    );
+  }
+}
