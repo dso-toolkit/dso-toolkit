a13ee3323a775ad04e2bb4157fc2796569586bfe
Pieter Verheij <pieter.verheij@infoprojects.nl>
2021-01-07 17:50:42 +0100

    #877 : latest greatest

--- packages/core/src/components/date-picker/date-picker.tsx
+++ packages/core/src/components/date-picker/date-picker.tsx
@@ -586,15 +586,7 @@ export class DsoDatePicker implements ComponentInterface {
                     </select>
                     <div class="dso-date__select-label" aria-hidden="true">
                       <span>{this.localization.monthNamesShort[focusedMonth]}</span>
-                      <svg
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M8.12 9.29L12 13.17l3.88-3.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0L6.7 10.7c-.39-.39-.39-1.02 0-1.41.39-.38 1.03-.39 1.42 0z" />
-                      </svg>
+                      <dso-icon icon="chevron-down"></dso-icon>
                     </div>
                   </div>
 
@@ -611,15 +603,7 @@ export class DsoDatePicker implements ComponentInterface {
                     </select>
                     <div class="dso-date__select-label" aria-hidden="true">
                       <span>{this.focusedDay.getFullYear()}</span>
-                      <svg
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M8.12 9.29L12 13.17l3.88-3.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0L6.7 10.7c-.39-.39-.39-1.02 0-1.41.39-.38 1.03-.39 1.42 0z" />
-                      </svg>
+                      <dso-icon icon="chevron-down"></dso-icon>
                     </div>
                   </div>
                 </div>
@@ -631,16 +615,7 @@ export class DsoDatePicker implements ComponentInterface {
                     disabled={prevMonthDisabled}
                     type="button"
                   >
-                    <svg
-                      aria-hidden="true"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="21"
-                      height="21"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M14.71 15.88L10.83 12l3.88-3.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L8.71 11.3c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0 .38-.39.39-1.03 0-1.42z" />
-                    </svg>
+                    <dso-icon icon="chevron-left"></dso-icon>
                     <span class="dso-date__vhidden">{this.localization.prevMonthLabel}</span>
                   </button>
                   <button
@@ -649,16 +624,7 @@ export class DsoDatePicker implements ComponentInterface {
                     disabled={nextMonthDisabled}
                     type="button"
                   >
-                    <svg
-                      aria-hidden="true"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="21"
-                      height="21"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z" />
-                    </svg>
+                    <dso-icon icon="chevron-right"></dso-icon>
                     <span class="dso-date__vhidden">{this.localization.nextMonthLabel}</span>
                   </button>
                 </div>
