a96dd280d2fcf1d3e69a236d9456befb710d80b7
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2021-01-08 14:59:45 +0100

    React package

---
+++ tests/react-test/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { DsoAlert } from '@dso-toolkit/react';
+
+function App() {
+  return (
+    <DsoAlert status="danger">
+      Test
+    </DsoAlert>
+  );
+}
+
+export default App;
