6ca4aee7a66258e37b58e0d03fe2f03e3d9a43cc
Pieter Verheij <pieter.verheij@infoprojects.nl>
2021-02-11 09:21:03 +0100

    #935 react files

---
+++ packages/react/src/components.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@dso-toolkit/core';
+
+
+
+export const DsoAlert = /*@__PURE__*/createReactComponent<JSX.DsoAlert, HTMLDsoAlertElement>('dso-alert');
+export const DsoAttachmentsCounter = /*@__PURE__*/createReactComponent<JSX.DsoAttachmentsCounter, HTMLDsoAttachmentsCounterElement>('dso-attachments-counter');
+export const DsoBadge = /*@__PURE__*/createReactComponent<JSX.DsoBadge, HTMLDsoBadgeElement>('dso-badge');
+export const DsoDatePicker = /*@__PURE__*/createReactComponent<JSX.DsoDatePicker, HTMLDsoDatePickerElement>('dso-date-picker');
+export const DsoHighlightBox = /*@__PURE__*/createReactComponent<JSX.DsoHighlightBox, HTMLDsoHighlightBoxElement>('dso-highlight-box');
+export const DsoIcon = /*@__PURE__*/createReactComponent<JSX.DsoIcon, HTMLDsoIconElement>('dso-icon');
+export const DsoLabel = /*@__PURE__*/createReactComponent<JSX.DsoLabel, HTMLDsoLabelElement>('dso-label');
+export const DsoProgressBar = /*@__PURE__*/createReactComponent<JSX.DsoProgressBar, HTMLDsoProgressBarElement>('dso-progress-bar');
+export const DsoWhitebox = /*@__PURE__*/createReactComponent<JSX.DsoWhitebox, HTMLDsoWhiteboxElement>('dso-whitebox');
