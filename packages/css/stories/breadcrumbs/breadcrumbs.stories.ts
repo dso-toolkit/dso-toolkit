ed926fc258f7fd36fada1524901bd20469c704f2
Dion Verhelst <dionverhelst@infoprojects.nl>
2021-05-21 12:04:10 +0200

    #1107.Storybook-HTML-Breadcrumbs

--- packages/css/stories/breadcrumbs/breadcrumbs.stories.ts
+++ packages/css/stories/breadcrumbs/breadcrumbs.stories.ts
@@ -1,4 +1,4 @@
-import { storiesOfBreadcrumbs, BreadcrumbsArgs, BreadcrumbsTemplateFn as BreadcrumbsTemplateFn } from '@dso-toolkit/stories';
+import { storiesOfBreadcrumbs, BreadcrumbsArgs, BreadcrumbsTemplateFn } from '@dso-toolkit/stories';
 import { storiesOf } from '@storybook/web-components';
 import { html, TemplateResult } from 'lit-html';
 import { ifDefined } from 'lit-html/directives/if-defined';
@@ -10,24 +10,24 @@ const template: BreadcrumbsTemplateFn<TemplateResult> = ({ breadcrumbs }: Breadc
   <nav aria-label="U bevindt zich hier:">
     <ol class="breadcrumb" itemscope itemtype="//schema.org/BreadcrumbList">
       ${breadcrumbs.map((breadcrumb, index) => {
-        const active = index === breadcrumbs.length - 1;
+        const last = index === breadcrumbs.length - 1;
 
         return html`
           <li
-            class="${ifDefined(active ? 'active' : undefined)}"
-            aria-current="${ifDefined(active ? 'page' : undefined)}"
+            class=${ifDefined(last ? 'active' : undefined)}
+            aria-current=${ifDefined(last ? 'page' : undefined)}
             itemscope=""
             itemprop="itemListElement"
             itemtype="//schema.org/ListItem"
           >
-            ${index === breadcrumbs.length - 1
+            ${last
               ? html`
                 <span itemprop="name">${breadcrumb.label}</span>
               `
               : html`
                 <a
                   itemprop="item"
-                  href="${breadcrumb.url}"
+                  href=${breadcrumb.url}
                 >
                   <span itemprop="name">
                     ${breadcrumb.label}
@@ -35,7 +35,7 @@ const template: BreadcrumbsTemplateFn<TemplateResult> = ({ breadcrumbs }: Breadc
                 </a>
               `
             }
-            <meta itemprop="position" content="${breadcrumb.position}">
+            <meta itemprop="position" content=${breadcrumb.position}>
           </li>
         `;
       })}
