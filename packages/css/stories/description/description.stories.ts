0d31937a73d6252a6d4b4c6789f819b31f541a46
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2021-05-26 16:18:52 +0200

    #1109 Description

--- packages/css/stories/description/description.stories.ts
+++ packages/css/stories/description/description.stories.ts
@@ -1,4 +1,4 @@
-import { storiesOfDescription, DescriptionArgs, DescriptionTemplateFn } from '@dso-toolkit/stories';
+import { storiesOfDescription, Description, DescriptionArgs, DescriptionExampleArgs, DescriptionExampleTemplateFn } from '@dso-toolkit/stories';
 import { storiesOf } from '@storybook/web-components';
 import { html, TemplateResult } from 'lit-html';
 import { ifDefined } from 'lit-html/directives/if-defined';
@@ -6,28 +6,31 @@ import { ifDefined } from 'lit-html/directives/if-defined';
 // @ts-ignore
 import readme from './readme.md';
 
-const template: DescriptionTemplateFn<TemplateResult> = ({ textBefore, textAfter, term, content, open, identifier }: DescriptionArgs) => html`
-  ${textBefore}
+const descriptionTemplate = ({ id, open, term, content }: Description) => html`
   <a
-    id="${identifier}-term"
-    href="${identifier}-content"
+    id="${id}-term"
+    href="${id}-content"
     class="dso-description-term ${open ? `dso-open` : ''}"
     aria-expanded="${ifDefined(open ? 'true' : 'false')}"
-    aria-controls="${identifier}-content">
+    aria-controls="${id}-content">
     ${term}
   </a>
-  <span id="${identifier}-content" class="dso-description-content">
+  <span id="${id}-content" class="dso-description-content">
     ${content}
-    <a href="${identifier}-term">
+    <a href="${id}-term">
       <span class="sr-only">Verbergen</span>
     </a>
   </span>
-  ${textAfter}
+`;
+
+const template: DescriptionExampleTemplateFn<TemplateResult> = ({ textBefore, id, term, content, open, textAfter }: DescriptionExampleArgs) => html`
+  ${textBefore} ${descriptionTemplate({ id, term, content, open })} ${textAfter}
 `;
 
 storiesOfDescription<TemplateResult>({
   module,
   storiesOf,
   readme,
-  template
+  descriptionTemplate: ({ id, term, content, open }: DescriptionArgs) => descriptionTemplate({ id, term, content, open }),
+  exampleTemplate: template
 });
--- packages/stories/src/stories/description.ts
+++ packages/stories/src/stories/description.ts
@@ -1,53 +1,53 @@
 import { bindTemplate, Parameters, TemplateFn } from '@core';
 import { v4 as uuidv4 } from 'uuid';
 
+export interface Description {
+  id: string;
+  term: string;
+  content: string;
+  open: boolean;
+}
+
 export interface DescriptionArgs {
-  textBefore: string,
-  textAfter: string,
-  term: string,
-  content: string,
-  open?: boolean,
-  identifier: string
+  id: string;
+  term: string;
+  content: string;
+  open: boolean;
 }
 
 export interface DescriptionTemplateFn<TemplateFnReturnType> extends TemplateFn<DescriptionArgs, TemplateFnReturnType> {
 }
 
-export interface DescriptionParameters<TemplateFnReturnType> extends Parameters<DescriptionArgs, TemplateFnReturnType> {
+export interface DescriptionExampleArgs {
+  textBefore: string,
+  textAfter: string,
+  id: string;
+  term: string;
+  content: string;
+  open: boolean;
+}
+
+export interface DescriptionExampleTemplateFn<TemplateFnReturnType> extends TemplateFn<DescriptionExampleArgs, TemplateFnReturnType> {
+}
+
+export interface DescriptionParameters<TemplateFnReturnType> extends Omit<Parameters<DescriptionExampleArgs, TemplateFnReturnType>, 'template'> {
+  exampleTemplate: DescriptionExampleTemplateFn<TemplateFnReturnType>;
+  descriptionTemplate: DescriptionTemplateFn<TemplateFnReturnType>;
 }
 
 export function storiesOfDescription<TemplateFnReturnType>({
   module: mainModule,
   storiesOf,
   readme,
-  template
+  exampleTemplate,
+  descriptionTemplate
 }: DescriptionParameters<TemplateFnReturnType>) {
   const stories = storiesOf('Description', mainModule)
     .addParameters({
       docs: {
         page: readme
       },
-      args: {
-        textBefore: 'Om interpretatieproblemen te voorkomen heeft het',
-        textAfter: 'aanbevelingen opgesteld over welke tags geldig zijn en hoe ze moeten worden geïnterpreteerd. De oorspronkelijke aanbeveling is een aantal malen geactualiseerd in verband met verdere ontwikkeling van HTML.',
-        term: 'W3C',
-        content: 'World Wide Web Consortium',
-        identifier: uuidv4(),
-        open: false
-      },
       argTypes: {
-        textBefore: {
-          control: {
-            type: 'text',
-            required: true
-          }
-        },
-        textAfter: {
-          control: {
-            type: 'text',
-            required: true
-          }
-        },
         term: {
           control: {
             type: 'text',
@@ -60,7 +60,7 @@ export function storiesOfDescription<TemplateFnReturnType>({
             required: true
           }
         },
-        identifier: {
+        id: {
           control: {
             type: 'text',
             required: true
@@ -75,7 +75,44 @@ export function storiesOfDescription<TemplateFnReturnType>({
     });
 
   stories.add(
+    'example',
+    bindTemplate(exampleTemplate),
+    {
+      argTypes: {
+        textBefore: {
+          control: {
+            type: 'text',
+            required: true
+          }
+        },
+        textAfter: {
+          control: {
+            type: 'text',
+            required: true
+          }
+        }
+      },
+      args: {
+        textBefore: 'Om interpretatieproblemen te voorkomen heeft het',
+        textAfter: 'aanbevelingen opgesteld over welke tags geldig zijn en hoe ze moeten worden geïnterpreteerd. De oorspronkelijke aanbeveling is een aantal malen geactualiseerd in verband met verdere ontwikkeling van HTML.',
+        id: uuidv4(),
+        term: 'W3C',
+        content: 'World Wide Web Consortium',
+        open: false
+      }
+    }
+  );
+
+  stories.add(
     'description',
-    bindTemplate(template)
+    bindTemplate(descriptionTemplate),
+    {
+      args: {
+        id: uuidv4(),
+        term: 'WWW',
+        content: 'World Wide Web',
+        open: false
+      }
+    }
   );
 }
