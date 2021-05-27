e41aba0cfd53546914bfe2be903b3c74d166372a
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2021-05-27 10:50:52 +0200

    @storybook/theming en logo's voor CSS en WC

---
+++ packages/core/.storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+import logo from './toolkit-web.svg';
+
+export default create({
+  base: 'light',
+  brandImage: logo
+});
---
+++ packages/css/.storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+import logo from './toolkit-css.svg';
+
+export default create({
+  base: 'light',
+  brandImage: logo
+});
