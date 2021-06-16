339a7020e010d27aa57eb3ce4290f622cae1282d
Pieter Verheij <pieter.verheij@infoprojects.nl>
2021-06-16 15:02:54 +0200

    Banner component (nog zonder icon)

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -15,7 +15,7 @@ module.exports = {
     '@storybook/addon-a11y'
   ],
   stories: [
-    '../src/**/*.stories.ts'
+    '../src/**/*.stories.(ts|tsx)'
   ],
   previewHead: head => process.env.DSO_ENV === 'development' // Todo: 'production'
     ? (`
---
+++ packages/react/src/components/banner/banner.template.tsx
@@ -0,0 +1,29 @@
+import { Banner } from '@dso-toolkit/sources';
+import * as React from 'react';
+
+import { DsoBanner } from '../..';
+// import { iconTemplate } from '../icon/icon.template';
+
+export function bannerTemplate({
+  status,
+  richContent,
+  onClick
+}: Banner<JSX.Element>) {
+  return (
+    <DsoBanner
+      status={status}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            {richContent}
+            <button type="button" className="btn btn-link" onClick={e => onClick(e.nativeEvent)}>
+              <span className="sr-only">Sluiten</span>
+              {/* ${iconTemplate({ icon: 'times' })} */}
+            </button>
+          </div>
+        </div>
+      </div>
+    </DsoBanner>
+  );
+}
