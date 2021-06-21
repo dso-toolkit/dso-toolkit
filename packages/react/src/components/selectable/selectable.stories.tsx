56914121c7ba41938b982df7145ff2f7b862cb13
Pieter Verheij <pieter.verheij@infoprojects.nl>
2021-06-21 15:26:09 +0200

    Tooltip en selectable fixje

--- packages/react/src/components/selectable/selectable.stories.tsx
+++ packages/react/src/components/selectable/selectable.stories.tsx
@@ -1,6 +1,8 @@
 import { storiesOfSelectable } from '@dso-toolkit/sources';
 import { storiesOf } from '@storybook/react';
 
+import * as React from 'react';
+
 import readme from './readme.md';
 import { selectableTemplate } from './selectable.template';
 
