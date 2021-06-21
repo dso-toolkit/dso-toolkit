bc52b02c141d470d8e62898190ff2a80e1093cae
Pieter Verheij <pieter.verheij@infoprojects.nl>
2021-06-21 16:51:13 +0200

    date-picker

---
+++ packages/react/src/components/date-picker/date-picker.template.tsx
@@ -0,0 +1,39 @@
+import { DatePicker } from '@dso-toolkit/sources';
+import * as React from 'react';
+
+import { DsoDatePicker } from '../..';
+
+// import { DsoDatePickerChangeEvent } from './date-picker';
+
+export function datePickerTemplate({ id, onDateChange, value, min, max, disabled }: DatePicker) {
+  return (
+    <DsoDatePicker
+      dateChange={(e: CustomEvent<DsoDatePickerChangeEvent>) => onDateChange(e)}
+      identifier={id}
+      value={value}
+      min={min}
+      max={max}
+      disabled={disabled}
+    ></DsoDatePicker>
+  );
+}
+
+export function datePickerWithLabelTemplate(datePicker: TemplateResult, id: string, label: string) {
+  return (
+    <>
+      <label for={id}>{label}</label>
+      {datePicker}
+    </>
+  );
+}
+
+export function datePickerShowByScriptingTemplate(datePicker: TemplateResult, id: string) {
+  return (
+    <>
+      {datePicker}
+      <button type="button" onClick={() => document.querySelector<HTMLDsoDatePickerElement>(`DsoDatePicker[identifier="${id}"]`)?.show()}>
+        Open
+      </button>
+    </>
+  );
+}
