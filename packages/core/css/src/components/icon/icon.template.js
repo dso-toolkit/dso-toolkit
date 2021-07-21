ddfc0261a02c21c7bae4437586968696d17c6fde
Dion Verhelst <dionverhelst@infoprojects.nl>
2021-07-21 16:11:28 +0200

    #1173-Webcomponent-Dropdown-button
    - verwijdering .js files die vorige commit zijn meegekomen

--- packages/core/css/src/components/icon/icon.template.js
+++
@@ -1,8 +0,0 @@
-import { html } from 'lit-html';
-export function iconTemplate({ icon }) {
-  return html `
-    <svg class="di di-${icon}">
-      <use href="dso-icons.svg#${icon}" />
-    </svg>
-  `;
-}
