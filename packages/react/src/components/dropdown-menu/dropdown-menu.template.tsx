467909b194b614929c2688afb80920814f3f5599
Ben Heldoorn <ben@infoprojects.nl>
2021-07-23 13:02:24 +0200

    #1173 dropdown react storybook

---
+++ packages/react/src/components/dropdown-menu/dropdown-menu.template.tsx
@@ -0,0 +1,20 @@
+import { DropdownMenu } from "@dso-toolkit/sources";
+import * as React from "react";
+
+import { DsoDropdownMenu } from "../..";
+
+export function dropdownMenuTemplate({
+  button,
+  dropdownAlign,
+  isCheckable,
+  children,
+}: DropdownMenu<JSX.Element>) {
+  return (
+    <DsoDropdownMenu dropdown-align={dropdownAlign} checkable={isCheckable}>
+      <button type="button" className={`dso-${button.variant}`} slot="button">
+        <span>{button.label}</span>
+      </button>
+      <div className="dso-dropdown-menu">{children}</div>
+    </DsoDropdownMenu>
+  );
+}
