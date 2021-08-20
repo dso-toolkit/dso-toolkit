21f170d04e1122a2772e9d76f1726fbe51fe7c22
Ben Heldoorn <ben@infoprojects.nl>
2021-08-20 08:31:44 +0200

    #1179 realistischer voorbeeld

--- packages/sources/src/components/toggletip/toggletip.stories.ts
+++ packages/sources/src/components/toggletip/toggletip.stories.ts
@@ -28,8 +28,14 @@ export function storiesOfToggletip<TemplateFnReturnType>(
       args: {
         children: `
           <div class="dso-rich-content">
-            <h2>Test</h2>
-            <p>Hoi <a href="#daar">daar</a>.</p>
+            <h2>Samenspel tussen wetgever en ontwikkelaars</h2>
+            <p>
+              Het DSO wordt ontwikkeld door het programma <a href="https://aandeslagmetdeomgevingswet.nl/digitaal-stelsel/">
+              Aan de slag met de Omgevingswet</a> in samenwerking met haar partners. De ontwikkeling vraagt een intensief samenspel
+              tussen de wetgever en de ontwikkelpartners van het DSO. Enerzijds moeten wetgever en de ontwikkelpartners zorgen dat
+              alles wat in de wet- en regelgeving wordt geregeld ook makkelijk toegankelijk is of kan worden. Anderzijds moeten zij
+              zorgen dat alles wat digitaal wordt gerealiseerd, ook juridisch verankerd is of kan worden.
+            </p>
           </div>
         `,
       },
