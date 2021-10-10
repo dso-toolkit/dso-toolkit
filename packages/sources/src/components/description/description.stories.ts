f072eb3ae094da67b05ecccd6daec18b59ba4310
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2021-10-10 14:41:23 +0200

    #1271 Nieuwe variant "note" voor Description

--- packages/sources/src/components/description/description.stories.ts
+++ packages/sources/src/components/description/description.stories.ts
@@ -1,14 +1,14 @@
 import { Args } from '@storybook/addons';
-import { v4 as uuidv4 } from 'uuid';
 
 import { bindTemplate, ArgsError, StorybookParameters } from '../../stories-helpers';
 
-import { DescriptionArgs, descriptionArgsMapper, descriptionArgTypes } from './description.args';
+import { descriptionArgsMapper, descriptionArgTypes, DescriptionExampleArgs, descriptionExampleArgTypes } from './description.args';
+import { noteContent, termContent, descriptionExample } from './description.content';
 import { Description } from './description.models';
 
 export interface DescriptionParameters<TemplateFnReturnType> {
   descriptionTemplate: (descriptionProperties: Description) => TemplateFnReturnType;
-  exampleTemplate: (exampleProperties: { textBefore: string, description: TemplateFnReturnType, textAfter: string }) => TemplateFnReturnType;
+  exampleTemplate: (exampleData: ReturnType<typeof descriptionExample>) => TemplateFnReturnType;
 }
 
 export function storiesOfDescription<TemplateFnReturnType>(
@@ -22,43 +22,48 @@ export function storiesOfDescription<TemplateFnReturnType>(
     exampleTemplate
   }: DescriptionParameters<TemplateFnReturnType>
 ) {
-  const template = (a: Args | undefined): TemplateFnReturnType => {
-    if (!a) {
-      throw new ArgsError();
-    }
-
-    const args = a as DescriptionArgs;
-
-    return exampleTemplate({
-      textAfter: args.textAfter,
-      description: descriptionTemplate(descriptionArgsMapper(args)),
-      textBefore: args.textBefore
-    });
-  };
-
   const stories = storiesOf('Description', mainModule)
     .addParameters({
       docs: {
         page: readme
-      },
-      argTypes: descriptionArgTypes,
-      args: {
-        textBefore: 'Om interpretatieproblemen te voorkomen heeft het',
-        textAfter: 'aanbevelingen opgesteld over welke tags geldig zijn en hoe ze moeten worden geïnterpreteerd. De oorspronkelijke aanbeveling is een aantal malen geactualiseerd in verband met verdere ontwikkeling van HTML.',
-        id: uuidv4(),
-        term: 'W3C',
-        content: 'World Wide Web Consortium',
-        open: false
       }
     });
 
   stories.add(
-    'example',
-    template
+    'term',
+    bindTemplate(descriptionArgsMapper, descriptionTemplate),
+    {
+      argTypes: descriptionArgTypes,
+      args: termContent
+    }
   );
 
   stories.add(
-    'description',
-    bindTemplate(descriptionArgsMapper, descriptionTemplate)
+    'note',
+    bindTemplate(descriptionArgsMapper, descriptionTemplate),
+    {
+      argTypes: descriptionArgTypes,
+      args: noteContent
+    }
+  );
+
+  stories.add(
+    'example',
+    (a: Args | undefined) => {
+      if (!a) {
+        throw new ArgsError();
+      }
+
+      const args = a as DescriptionExampleArgs;
+
+      return exampleTemplate(descriptionExample(args.openTerm, args.openNote));
+    },
+    {
+      argTypes: descriptionExampleArgTypes,
+      args: {
+        openTerm: false,
+        openNote: true
+      }
+    }
   );
 }
