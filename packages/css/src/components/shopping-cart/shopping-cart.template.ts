b51342bfc13029324c41247075fb8b994643c674
Pieter Verheij <pieter.verheij@infoprojects.nl>
2022-05-18 10:31:01 +0200

    tooltips

--- packages/css/src/components/shopping-cart/shopping-cart.template.ts
+++ packages/css/src/components/shopping-cart/shopping-cart.template.ts
@@ -5,6 +5,7 @@ import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import { buttonTemplate } from '../button/button.template';
 import { iconTemplate } from '../icon/icon.template';
+import { tooltipTemplate } from '../tooltip/tooltip.template';
 
 function accumulateItems(items: ShoppingCartItem[]) {
   return items.reduce<ShoppingCartItem[]>((t, item) => t.concat(item), []);
@@ -64,6 +65,10 @@ export function shoppingCartTemplate({ collapsed, hideSummary, removeAll, isOpen
                   <button type="button" class="dso-delete">
                     ${iconTemplate({ icon: 'trash' })}
                     <span class="sr-only">Verwijder alle werkzaamheden/activiteiten</span>
+                    <div class="tooltip top fade in" role="tooltip">
+                      <div class="tooltip-arrow"></div>
+                      <div class="tooltip-inner">Alles verwijderen</div>
+                    </div>
                   </button>
                   `
                   : nothing
@@ -118,8 +123,13 @@ export function shoppingCartTemplate({ collapsed, hideSummary, removeAll, isOpen
                       }
                       ${!item.edit
                         ? html`
-                          <button type="button" class="dso-delete" title="Verwijder">
+                          <button type="button" class="dso-delete">
                             ${iconTemplate({ icon: 'trash' })}
+                            <span class="sr-only">Verwijder ${item.label}</span>
+                            <div class="tooltip top fade in" role="tooltip">
+                              <div class="tooltip-arrow"></div>
+                              <div class="tooltip-inner">Verwijder</div>
+                            </div>
                           </button>
                         `
                         : nothing
@@ -136,6 +146,11 @@ export function shoppingCartTemplate({ collapsed, hideSummary, removeAll, isOpen
                                 ${subitem.label}
                                 <button type="button" class="dso-delete" title="Verwijder">
                                   ${iconTemplate({ icon: 'trash' })}
+                                  <span class="sr-only">Verwijder ${item.label}</span>
+                                  <div class="tooltip top fade in" role="tooltip">
+                                    <div class="tooltip-arrow"></div>
+                                    <div class="tooltip-inner">Verwijder</div>
+                                  </div>
                                 </button>
                               </li>
                             `)}
