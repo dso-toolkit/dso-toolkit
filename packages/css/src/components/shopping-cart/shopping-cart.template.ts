b598354b2a0548961bc4c86abea7f86c14be6263
Pieter Verheij <pieter.verheij@infoprojects.nl>
2022-05-18 16:27:08 +0200

    geen tooltip in css storybook

--- packages/css/src/components/shopping-cart/shopping-cart.template.ts
+++ packages/css/src/components/shopping-cart/shopping-cart.template.ts
@@ -5,7 +5,6 @@ import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import { buttonTemplate } from '../button/button.template';
 import { iconTemplate } from '../icon/icon.template';
-import { tooltipTemplate } from '../tooltip/tooltip.template';
 
 function accumulateItems(items: ShoppingCartItem[]) {
   return items.reduce<ShoppingCartItem[]>((t, item) => t.concat(item), []);
@@ -65,10 +64,6 @@ export function shoppingCartTemplate({ collapsed, hideSummary, removeAll, isOpen
                   <button type="button" class="dso-delete">
                     ${iconTemplate({ icon: 'trash' })}
                     <span class="sr-only">Verwijder alle werkzaamheden/activiteiten</span>
-                    <div class="tooltip top fade in" role="tooltip">
-                      <div class="tooltip-arrow"></div>
-                      <div class="tooltip-inner">Alles verwijderen</div>
-                    </div>
                   </button>
                   `
                   : nothing
@@ -126,10 +121,6 @@ export function shoppingCartTemplate({ collapsed, hideSummary, removeAll, isOpen
                           <button type="button" class="dso-delete">
                             ${iconTemplate({ icon: 'trash' })}
                             <span class="sr-only">Verwijder ${item.label}</span>
-                            <div class="tooltip top fade in" role="tooltip">
-                              <div class="tooltip-arrow"></div>
-                              <div class="tooltip-inner">Verwijder</div>
-                            </div>
                           </button>
                         `
                         : nothing
@@ -147,10 +138,6 @@ export function shoppingCartTemplate({ collapsed, hideSummary, removeAll, isOpen
                                 <button type="button" class="dso-delete" title="Verwijder">
                                   ${iconTemplate({ icon: 'trash' })}
                                   <span class="sr-only">Verwijder ${item.label}</span>
-                                  <div class="tooltip top fade in" role="tooltip">
-                                    <div class="tooltip-arrow"></div>
-                                    <div class="tooltip-inner">Verwijder</div>
-                                  </div>
                                 </button>
                               </li>
                             `)}
