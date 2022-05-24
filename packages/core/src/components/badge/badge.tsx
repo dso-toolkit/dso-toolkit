1e5ca933f6ae562c4f7284f1cd03953b52e58a82
Dion Verhelst <dionverhelst@infoprojects.nl>
2022-05-24 09:19:13 +0200

    #1570.sr-only-tekst-uit-badges-verwijderen

--- packages/core/src/components/badge/badge.tsx
+++ packages/core/src/components/badge/badge.tsx
@@ -10,18 +10,9 @@ export class Badge {
   @Prop()
   status?: 'primary' | 'success' | 'info' | 'warning' | 'danger' | 'outline';
 
-  private static statusMap = new Map<string, string>([
-    ['success', 'Gelukt'],
-    ['info', 'Opmerking'],
-    ['warning', 'Waarschuwing'],
-    ['danger', 'Fout'],
-    ['outline', 'Rand']
-  ]);
-
   render() {
     return (
       <span class={clsx('dso-badge', { [`badge-${this.status}`]: this.status })}>
-        <span class="sr-only">{this.status ? Badge.statusMap.get(this.status) : 'Badge'}: </span>
         <slot></slot>
       </span>
     );
