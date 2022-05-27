d149b2a731971e301be77c9059a50fef6c592f22
Ad van Pinxteren <ad.van.pinxteren@gmail.com>
2022-05-27 08:28:09 +0200

    #1548 Toegankelijkheidsissues met tooltip en toggletip
    * import from explicit .js

--- packages/css/src/components/tooltip/tooltip.template.ts
+++ packages/css/src/components/tooltip/tooltip.template.ts
@@ -1,6 +1,6 @@
 import { Tooltip } from '@dso-toolkit/sources';
 import { html } from 'lit-html';
-import { ifDefined } from 'lit-html/directives/if-defined';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 export function tooltipTemplate({ position, descriptive }: Tooltip) {
   return html`
