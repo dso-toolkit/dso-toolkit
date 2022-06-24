b2b20aae7cb20467a321d0784e04524a1bb8ae08
Pieter Verheij <pieter.verheij@infoprojects.nl>
2022-06-24 10:21:45 +0200

    #1617 Form Group: Textarea naar @dso-toolkit/css

--- packages/sources/src/components/form/form.models.ts
+++ packages/sources/src/components/form/form.models.ts
@@ -2,5 +2,6 @@ export * from './models/form-group-checkboxes.model';
 export * from './models/form-group-confirm.model';
 export * from './models/form-group-input.model';
 export * from './models/form-group-radios.model';
+export * from './models/form-group-textarea.model';
 export * from './models/form-group.base-model';
 export * from './models/form.model';
--- packages/sources/src/components/form/models/form-group.model.ts
+++ packages/sources/src/components/form/models/form-group.model.ts
@@ -2,5 +2,6 @@ import { FormGroupCheckboxes } from './form-group-checkboxes.model';
 import { FormGroupConfirm } from './form-group-confirm.model';
 import { FormGroupInput, FormGroupInputDate } from './form-group-input.model';
 import { FormGroupRadios } from './form-group-radios.model';
+import { FormGroupTextarea } from './form-group-textarea.model';
 
-export type FormGroup<TemplateFnReturnType> = FormGroupCheckboxes<TemplateFnReturnType> | FormGroupConfirm<TemplateFnReturnType> | FormGroupInput<TemplateFnReturnType> | FormGroupInputDate<TemplateFnReturnType> | FormGroupRadios<TemplateFnReturnType>;
+export type FormGroup<TemplateFnReturnType> = FormGroupCheckboxes<TemplateFnReturnType> | FormGroupConfirm<TemplateFnReturnType> | FormGroupInput<TemplateFnReturnType> | FormGroupInputDate<TemplateFnReturnType> | FormGroupRadios<TemplateFnReturnType> | FormGroupTextarea<TemplateFnReturnType>;
