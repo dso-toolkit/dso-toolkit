2c6c5d498549a305aca3d3eea1b5ed771845b70e
Pieter Verheij <pieter.verheij@infoprojects.nl>
2022-06-30 10:14:44 +0200

    component en css refactoring

--- packages/css/src/components/form/form-groups/files/form-group-files.template.ts
+++ packages/css/src/components/form/form-groups/files/form-group-files.template.ts
@@ -1,4 +1,4 @@
-import { FormGroupFiles } from '@dso-toolkit/sources';
+import { AlertType, FormGroupFiles } from '@dso-toolkit/sources';
 import { html, nothing, TemplateResult } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
@@ -6,6 +6,9 @@ import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { infoButtonTemplate } from '../../../info-button/info-button.template';
 import { infoTemplate } from '../../../info/info.template';
 import { iconTemplate } from '../../../icon/icon.template';
+import { alertTemplate } from '../../../alert/alert.template';
+import { buttonTemplate } from '../../../button/button.template';
+import { selectableTemplate } from '../../../selectable/selectable.template';
 
 export function formGroupFilesTemplate(formGroup: FormGroupFiles<TemplateResult>): TemplateResult {
   const errorTextId = `${formGroup.id}-error-text`;
@@ -19,7 +22,7 @@ export function formGroupFilesTemplate(formGroup: FormGroupFiles<TemplateResult>
     .join(' ') || undefined;
 
   return html`
-    <div class="form-group dso-files ${classMap({ 'dso-required': !!formGroup.required, [`dso-${formGroup.state}`]: !!formGroup.state })}">
+    <fieldset class="form-group dso-files ${classMap({ 'dso-required': !!formGroup.required, [`dso-${formGroup.state}`]: !!formGroup.state })}">
       <div class="dso-label-container">
         <label for=${formGroup.id} class="control-label">
           ${formGroup.label}
@@ -36,9 +39,36 @@ export function formGroupFilesTemplate(formGroup: FormGroupFiles<TemplateResult>
       <div class="dso-field-container">
         <ul class="dso-filelist">
           ${formGroup.files.map(file =>
-            html `<li>file hier</li>`
+            html`
+              <li>
+                <div class="dso-filename" id="">${file.filename}</div>
+                ${selectableTemplate({ id: '', value:'', type: 'checkbox', label: 'Vertrouwelijk' })}
+                ${file.confidential
+                  ? iconTemplate({ icon: 'status-warning' })
+                  : nothing
+                }
+                ${buttonTemplate({ label: 'Sluiten', variant: null, modifier: 'btn btn-link dso-remove', icon: { icon: 'times' }, iconMode: 'only' })}
+              </li>`
           )}
         </ul>
+        ${formGroup.files.some(file => !!file.confidential)
+          ? alertTemplate({ status: AlertType.Warning, message: formGroup.warning })
+          : nothing
+        }
+        <div class="dso-file-upload">
+          <input
+            type="file"
+            aria-describedby=${ifDefined(ariaDescribedBy)}
+            ?id=${formGroup.id}
+            ?disabled=${formGroup.disabled}
+          />
+          <label
+            class="dso-${ formGroup.addFileButtonVariant }"
+            for="{ ${formGroup.id} }">
+            ${iconTemplate({ icon: 'plus' })}
+            <span>Document toevoegen</span>
+          </label>
+        </div>
 
         ${formGroup.errorText
           ? html`
@@ -47,6 +77,7 @@ export function formGroupFilesTemplate(formGroup: FormGroupFiles<TemplateResult>
           : nothing
         }
       </div>
-    </div>
+    </fieldset>
   `;
 }
+
--- packages/sources/src/components/form/models/form-group-files.model.ts
+++ packages/sources/src/components/form/models/form-group-files.model.ts
@@ -3,4 +3,29 @@ import { Icon } from '../../..';
 import { FormGroupBase } from './form-group.base-model';
 
 export interface FormGroupFiles<TemplateFnReturnType> extends FormGroupBase<TemplateFnReturnType> {
+  group: 'files';
+  addFileButtonVariant: 'primary' | 'secondary';
+  files: FormGroupFilesFile[];
+  warning: string;
 }
+
+export interface FormGroupFilesFile {
+  filename: string;
+  confidential?: boolean;
+}
+
+// --
+
+// interface Persoon {
+//   naam: string;
+//   leeftijd: number;
+// }
+
+// const pieter: Persoon = {
+//   naam: 'pieter',
+//   leeftijd: 23
+// };
+
+// const thomas: Persoon = {
+
+// }
