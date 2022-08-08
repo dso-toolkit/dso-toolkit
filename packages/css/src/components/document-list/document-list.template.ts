8cc951bf9430299c05e8ba7afba0d162c26461f4
Dion Verhelst <dionverhelst@infoprojects.nl>
2022-08-08 08:35:56 +0200

    #1657.Document-Item-uitbreiden-met-container-voor-regels

--- packages/css/src/components/document-list/document-list.template.ts
+++ packages/css/src/components/document-list/document-list.template.ts
@@ -20,16 +20,69 @@ function documentListItemTemplate({
 }: DocumentListItem<TemplateResult>) {
   return html`
     <div class="dso-document-list-item">
-      <h2>${title}</h2>
-      <div class="dso-document-list-item-container">
-        <p class="dso-document-list-item-type">${type}</p>
-        <p class="dso-document-list-item-owner">${owner}</p>
-        <p class="dso-document-list-item-status">${status}</p>
+      <div class="dso-document-list-item-heading">
+        <h2>${title}</h2>
+        <div class="dso-document-list-item-container">
+          <p class="dso-document-list-item-type">${type}</p>
+          <p class="dso-document-list-item-owner">${owner}</p>
+          <p class="dso-document-list-item-status">${status}</p>
+          ${anchorTemplate({
+            url: "#",
+            label: "Hele document bekijken",
+            modifier: "dso-document-list-item-open-document",
+          })}
+        </div>
+      </div>
+      <div class="dso-document-list-item-content">
+        <p>Er zijn regels gemaakt die in de toekomst geldig worden op deze locatie:</p>
         ${anchorTemplate({
           url: "#",
-          label: "Hele document bekijken",
-          modifier: "dso-document-list-item-open-document",
+          label: "Toekomstige regels tonen",
         })}
+        <hr />
+        <small>Gevonden in: Hoofdstuk 2 Functietoedeling / Afdeling wonen</small>
+        <ul class="dso-link-list">
+          <li>
+            ${anchorTemplate({
+              url: "#",
+              label: "22.1 Functieomschrijving",
+            })}
+          </li>
+          <li>
+            ${anchorTemplate({
+              url: "#",
+              label: "22.2 Bouwregels",
+            })}
+          </li>
+          <li>
+            ${anchorTemplate({
+              url: "#",
+              label: "22.3 Nadere eisen",
+            })}
+          </li>
+          <li>
+            ${anchorTemplate({
+              url: "#",
+              label: "22.4 Ontheffing van de bouwregels",
+            })}
+          </li>
+        </ul>
+        <hr />
+        <small>Gevonden in: Hoofdstuk 2 Functietoedeling / Artikel 23 Watergang</small>
+        <ul class="dso-link-list">
+          <li>
+            ${anchorTemplate({
+              url: "#",
+              label: "23.1 Functieomschrijving",
+            })}
+          </li>
+          <li>
+            ${anchorTemplate({
+              url: "#",
+              label: "23.2 Regels voor doorgangbeperking",
+            })}
+          </li>
+        </ul>
       </div>
     </div>
   `;
