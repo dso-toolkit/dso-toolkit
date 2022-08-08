384d20940bba085a6895f37d4174c669a3e49dc0
Pieter Verheij <pieter.verheij@infoprojects.nl>
2022-08-08 14:30:11 +0200

    #1668 Altijd form tonen in voorbeeld

--- packages/css/src/components/form-buttons/form-buttons.template.ts
+++ packages/css/src/components/form-buttons/form-buttons.template.ts
@@ -1,26 +1,14 @@
 import { Button, FormButtons } from '@dso-toolkit/sources';
 import { html } from 'lit-html';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import { buttonTemplate } from '../button/button.template';
 
 export function formButtonsTemplate({ formModifier, buttons, asideButtons }: FormButtons) {
-  if (formModifier) {
-    return html`
-      <form class=${formModifier}>
-        <div class="dso-form-buttons">
-          ${asideButtons && asideButtons.length > 0 && html`
-            <div class="dso-aside">
-              ${asideButtons.map((button: Button) => buttonTemplate(button))}
-            </div>
-          `}
-
-          ${buttons.map((button: Button) => buttonTemplate(button))}
-        </div>
-      </form>
-    `;
-  }
-  else {
-    return html`
+  return html`
+    <form
+      class=${ifDefined(formModifier)}
+    >
       <div class="dso-form-buttons">
         ${asideButtons && asideButtons.length > 0 && html`
           <div class="dso-aside">
@@ -30,6 +18,6 @@ export function formButtonsTemplate({ formModifier, buttons, asideButtons }: For
 
         ${buttons.map((button: Button) => buttonTemplate(button))}
       </div>
-    `;
-  }
+    </form>
+  `;
 }
