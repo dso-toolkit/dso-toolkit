3fdf25b53af82e49301a328f85d8c626a4f939d8
Luke Verlaan <luke.verlaan@infoprojects.nl>
2022-08-26 10:03:16 +0200

    #1670 DsoToggletip: Error: button not found

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 * **react** React Storybook: Bij Date Picker worden de actions niet getoond ([#1664](https://github.com/dso-toolkit/dso-toolkit/issues/1664))
 * **css:** Als de tekst te lang is dan loopt ie over de 'Hele document bekijken'-knop heen ([#1652](https://github.com/dso-toolkit/dso-toolkit/issues/1652))
 * **css:** Viewer Grid: .document-list-item mist margin right voor .dso-document-list-item-type ([#1649](https://github.com/dso-toolkit/dso-toolkit/issues/1649))
+* **core** DsoToggletip: Error: button not found ([#1670](https://github.com/dso-toolkit/dso-toolkit/issues/1670))
 
 ### Changed
 * **core:** Afbeelding in dso-image-overlay alleen zoomable maken als de afbeelding niet in de container past ([#1695](https://github.com/dso-toolkit/dso-toolkit/issues/1695))
---
+++ cypress/integration/core/toggletip.spec.ts
@@ -0,0 +1,43 @@
+describe('Toggletip', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:56106/iframe.html?id=toggletip--toggletip');
+  });
+
+  function prepareComponent() {
+    cy
+      .get('dso-toggletip')
+      .as('dsoToggletip');
+
+    cy
+      .get('@dsoToggletip')
+      .shadow()
+      .find('dso-info-button')
+      .as('dsoInfoButton');
+
+    cy
+      .get('@dsoInfoButton')
+      .shadow()
+      .find('button')
+      .as('dsoButton');
+
+    cy
+      .get('@dsoToggletip')
+      .shadow()
+      .find('dso-tooltip')
+      .as('dsoTooltip');
+  }
+
+  it('should focus on button after escape', () => {
+    prepareComponent();
+
+    cy
+      .get('@dsoButton')
+      .click()
+      .get('@dsoTooltip')
+      .should('not.have.class', 'hidden')
+      .realPress('Tab')
+      .realPress('Escape')
+      .get('@dsoButton')
+      .should('have.focus');
+  });
+});
