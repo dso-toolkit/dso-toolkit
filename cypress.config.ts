5d259900b541e9074e50992c3dfe680159c7fdf6
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2022-10-24 13:23:28 +0200

    #1444 Prettier en ESLint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    // eslint-disable-next-line @typescript-eslint/no-empty-function -- The presence of this function marks the Cypress configuration
+    setupNodeEvents(_on, _config) {},
   },
-})
+});
