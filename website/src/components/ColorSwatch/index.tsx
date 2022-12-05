87d74ca43943c3deb8aea794e6f5f6b903710eca
Emvium <michielvanmensvoort@gmail.com>
2022-12-05 12:18:28 +0100

    Kleur aanpassingen en variabelen voor consistentie. Ook Jira-ticket dsot-131 meegepakt. Bosgroen 130 -> 140 omgezet

--- website/src/components/ColorSwatch/index.tsx
+++ website/src/components/ColorSwatch/index.tsx
@@ -6,23 +6,28 @@ interface Props {
   label: string;
   name: string;
   rgb: string;
+  textcolor: string;
 }
 
-export function ColorSwatch({ label, name, rgb }: Props) {
+export function ColorSwatch({ label, name, rgb, textcolor }: Props) {
   if (!styles[name]) {
     throw new Error(`No color exported for $${name}`);
+  } 
+  if  (!styles[textcolor]){
+    throw new Error(`No color exported for $${textcolor}`);
   }
 
   return (
-    <div>
-      <h4>{label}</h4>
-      <div style={{ backgroundColor: styles[name], width: "100%", height: "150px" }}></div>
-      <p style={{ marginBottom: 0 }}>
-        <code>{styles[name]}</code>
-      </p>
-      <p>
-        <code>{rgb}</code>
-      </p>
-    </div>
+      <div style={{ backgroundColor: styles[name], width: "150px", height: "150px", display: "inline-block", padding: "8px 8px", position: "relative" }}>
+        <h4 style={{ color: styles[textcolor] }}>{label}</h4>
+        <div style={{color: styles[textcolor], bottom: "0px", position: "absolute"}}>
+          <p style={{ marginBottom: 0 }}>
+            {styles[name]}
+          </p>
+          <p style={{ marginBottom: 8 }}>
+            {rgb}
+          </p>
+        </div>
+      </div>
   );
 }
