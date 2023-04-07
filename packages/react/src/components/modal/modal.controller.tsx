e97138a8214a52e909879c1fbe69ad96cc29b75f
Luke Verlaan <luke.verlaan@infoprojects.nl>
2023-04-07 09:31:21 +0200

    #1983 Controller voor het DsoModal component
    * React EventListeners

--- packages/react/src/components/modal/modal.controller.tsx
+++ packages/react/src/components/modal/modal.controller.tsx
@@ -5,13 +5,14 @@ import ReactDOM from "react-dom";
 import { DsoModal } from "../../components";
 
 export class ReactModalContentComponent implements ModalContentComponent<JSX.Element> {
-  private modalRef?: HTMLElement;
+  private modalRef = React.createRef<HTMLDsoModalElement>();
+
+  private eventListeners: Record<string, () => void> = {};
 
   component: JSX.Element;
 
-  constructor(component: JSX.Element, modalRef: HTMLElement | null) {
-    this.component = component;
-    this.modalRef = modalRef ?? undefined;
+  constructor(Modal: React.ForwardRefExoticComponent<React.RefAttributes<HTMLDsoModalElement>>) {
+    this.component = <Modal ref={this.modalRef} />;
   }
 
   open() {
@@ -19,42 +20,53 @@ export class ReactModalContentComponent implements ModalContentComponent<JSX.Ele
       ReactDOM.createPortal(this.component, document.body),
       document.body.appendChild(document.createDocumentFragment())
     );
+
+    Object.entries(this.eventListeners).forEach(([key, value]) => {
+      this.modalRef.current?.addEventListener(key, value);
+    });
   }
 
   close() {
+    Object.entries(this.eventListeners).forEach(([key, value]) => {
+      this.modalRef.current?.removeEventListener(key, value);
+    });
+
     document.body.lastChild?.remove();
   }
 
   addEventListener(eventName: string, fn: () => void): void {
-    this.modalRef?.addEventListener(eventName, fn);
+    this.modalRef.current?.addEventListener(eventName, fn);
+    this.eventListeners[eventName] = fn;
   }
 
   removeEventListener(eventName: string, fn: () => void): void {
-    this.modalRef?.addEventListener(eventName, fn);
+    this.modalRef.current?.removeEventListener(eventName, fn);
+    delete this.eventListeners[eventName];
   }
 }
 
 export class ModalController implements DsoModalController<JSX.Element> {
-  createInstance(content: ModalContent<JSX.Element>, options?: ModalOptions) {
+  createInstance(content: ModalContent<JSX.Element>, options?: ModalOptions): ReactModalContentComponent {
     return useModal(content, options);
   }
 }
 
-export function useModal({ title, body, footer }: ModalContent<JSX.Element>, options?: ModalOptions) {
-  const modalRef = React.useRef<HTMLDsoModalElement | null>(null);
-
-  const element = (
+export function useModal(
+  { title, body, footer }: ModalContent<JSX.Element>,
+  options?: ModalOptions
+): ReactModalContentComponent {
+  const Modal = React.forwardRef<HTMLDsoModalElement>((_props, ref) => (
     <DsoModal
       role={options?.role}
       modalTitle={title}
       showCloseButton={options?.showCloseButton ?? true}
       initialFocus={options?.initialFocus}
-      ref={modalRef}
+      ref={ref}
     >
       <div slot="body">{body}</div>
       {footer && <div slot="footer">{footer}</div>}
     </DsoModal>
-  );
+  ));
 
-  return new ReactModalContentComponent(element, modalRef.current);
+  return new ReactModalContentComponent(Modal);
 }
