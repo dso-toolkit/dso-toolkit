741f498ee4ec15a98da0937eeb9d874c1f0f9d81
Luke Verlaan <luke@infoprojects.local>
2023-06-05 12:25:44 +0200

    #2036 Slide-toggle ook drag bediening ondersteunen

--- packages/core/src/components/slide-toggle/slide-toggle.tsx
+++ packages/core/src/components/slide-toggle/slide-toggle.tsx
@@ -11,6 +11,38 @@ export class SlideToggle implements ComponentInterface {
   @Element()
   private host!: HTMLElement;
 
+  private toggleButton?: HTMLButtonElement;
+
+  private rect?: SVGElement;
+
+  private circle?: SVGElement;
+
+  private drag = false;
+
+  private colors = [
+    "#999999",
+    "#939694",
+    "#8E938F",
+    "#888F8A",
+    "#828C85",
+    "#7D8981",
+    "#77867C",
+    "#718377",
+    "#6B7F72",
+    "#667C6D",
+    "#607968",
+    "#5A7663",
+    "#55735E",
+    "#4F6F59",
+    "#496C54",
+    "#446950",
+    "#3E664B",
+    "#386346",
+    "#325F41",
+    "#2D5C3C",
+    "#275937",
+  ];
+
   @Prop()
   checked = false;
 
@@ -35,34 +67,124 @@ export class SlideToggle implements ComponentInterface {
   @Event()
   dsoActiveChange!: EventEmitter<SlideToggleActiveEvent>;
 
-  private handleSwitch(e: Event): void {
+  private handleSwitch(e: Event, checked: boolean): void {
     this.dsoActiveChange.emit({
       originalEvent: e,
-      checked: !this.checked,
+      checked,
     });
   }
 
+  private isToggleButton(element: EventTarget | null): element is HTMLButtonElement {
+    return element instanceof SVGElement && this.toggleButton?.contains(element) ? true : false;
+  }
+
+  private handleMouseDown = (e: MouseEvent) => {
+    if (!this.isToggleButton(e.target)) {
+      return;
+    }
+
+    this.drag = false;
+
+    document.addEventListener("mousemove", this.handleDrag);
+  };
+
+  private handleDrag = (e: MouseEvent) => {
+    this.drag = true;
+
+    if (!(this.circle instanceof SVGCircleElement) || !(this.rect instanceof SVGRectElement)) {
+      return;
+    }
+
+    this.circle.style.transition = "none";
+    this.rect.style.transition = "none";
+
+    const clientX = e.clientX - 20;
+
+    if (clientX <= 10) {
+      this.rect.style.fill = this.colors[0];
+      this.circle.style.transform = "translateX(10px)";
+    }
+
+    if (clientX >= 30) {
+      this.rect.style.fill = this.colors[this.colors.length - 1];
+      this.circle.style.transform = "translateX(30px)";
+    }
+
+    if (clientX > 10 && clientX < 30) {
+      this.rect.style.fill = this.colors[clientX - 11];
+      this.circle.style.transform = `translateX(${clientX}px)`;
+    }
+  };
+
+  private handleMouseUp = (e: MouseEvent) => {
+    document.removeEventListener("mousemove", this.handleDrag);
+
+    if (this.drag) {
+      if (!(this.circle instanceof SVGCircleElement) || !(this.rect instanceof SVGRectElement)) {
+        return;
+      }
+
+      this.circle.removeAttribute("style");
+      this.rect.removeAttribute("style");
+
+      const clientX = e.clientX - 20;
+
+      if (clientX > 20) {
+        this.handleSwitch(e, true);
+      }
+
+      if (clientX <= 20) {
+        this.handleSwitch(e, false);
+      }
+    } else if (!this.drag && this.isToggleButton(e.target)) {
+      this.handleSwitch(e, !this.checked);
+    }
+
+    this.drag = false;
+  };
+
   componentWillLoad(): void {
     this.hasVisibleLabel = this.host.querySelector("*") !== null;
+  }
+
+  componentDidLoad(): void {
+    document.addEventListener("mousedown", this.handleMouseDown);
+    document.addEventListener("mouseup", this.handleMouseUp);
+    this.toggleButton?.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        this.handleSwitch(event, !this.checked);
+      }
+    });
+  }
+
+  disconnectedCallback(): void {
+    document.removeEventListener("mousedown", this.handleMouseDown);
+    document.removeEventListener("mouseup", this.handleMouseUp);
+    document.removeEventListener("mousemove", this.handleDrag);
+    this.toggleButton?.removeEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        this.handleSwitch(event, !this.checked);
+      }
+    });
   }
 
   render() {
     return (
       <>
         <button
+          ref={(button) => (this.toggleButton = button)}
           id={this.identifier}
           role="switch"
           class="dso-slider"
           aria-checked={"" + this.checked}
           disabled={this.disabled}
-          onClick={(e) => this.handleSwitch(e)}
           {...(this.accessibleLabel ? { "aria-label": this.accessibleLabel } : {})}
           {...(this.labelledbyId ? { "aria-labelledby": this.labelledbyId } : {})}
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="20" viewBox="0 0 40 20">
             <g fill="none" fill-rule="evenodd">
-              <rect width="40" height="20" fill="currentColor" rx="10" />
-              <circle cy="10" r="8" fill="currentColor" />
+              <rect ref={(rect) => (this.rect = rect)} width="40" height="20" fill="currentColor" rx="10" />
+              <circle ref={(circle) => (this.circle = circle)} cy="10" r="8" fill="currentColor" />
             </g>
           </svg>
         </button>
