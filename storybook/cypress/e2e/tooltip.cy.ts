a1160dbb2b55a7231a59ecbf171fd45587a02ea6
Luke Verlaan <luke@infoprojects.local>
2023-06-05 12:48:08 +0200

    #2073 #Fixed: Tooltip: Met expliciete id relatie sluit de tooltip al zodra de cursor op de content staat
    tests

--- storybook/cypress/e2e/tooltip.cy.ts
+++ storybook/cypress/e2e/tooltip.cy.ts
@@ -12,13 +12,19 @@ describe("Tooltip", () => {
   // https://github.com/dmtrKovalenko/cypress-real-events/issues/247
   // realHover has issues since chrome 100.
 
-  // it('should show tooltip on hover on button and hide on escape key', () => {
-  //   prepareComponent();
+  it("should show tooltip on hover on button and hide on escape key", () => {
+    prepareComponent();
 
-  //   cy
-  //     .get('@dsoButton')
-  //     .realHover();
-  // });
+    cy.get("@dsoButton")
+      // .realHover()
+      .trigger("mouseover")
+      .find("dso-tooltip")
+      .should("not.have.class", "hidden")
+      .realPress("Escape")
+      .get("@dsoButton")
+      .find("dso-tooltip")
+      .should("have.class", "hidden");
+  });
 
   // Temporary test, this shouldn't be needed. Don't copy/paste this.
   it("should look ok", () => {
@@ -40,5 +46,26 @@ describe("Tooltip", () => {
       .get("@dsoButton")
       .find("dso-tooltip")
       .should("have.class", "hidden");
+  });
+
+  it("should not hide tooltip when hover is in tooltip, but outside button", () => {
+    cy.visit("http://localhost:45000/iframe.html?id=core-tooltip--as-sibling");
+
+    cy.get("#root").invoke("attr", "style", "min-height: 360px;");
+
+    cy.get("dso-tooltip")
+      .as("dsoTooltip")
+      .should("have.class", "hidden")
+      .prev()
+      .should("have.attr", "type", "button")
+      .as("dsoButton");
+
+    cy.get("@dsoButton")
+      .trigger("mouseenter")
+      .get("@dsoTooltip")
+      .should("not.have.class", "hidden")
+      .get("@dsoTooltip")
+      .realHover()
+      .should("not.have.class", "hidden");
   });
 });
