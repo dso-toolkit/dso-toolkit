71ed760eafe12c5ea9c9ff8869882ff0f60f7905
Luke Verlaan <luke@infoprojects.local>
2023-06-09 08:16:31 +0200

    #2110 h5 zonder marges
    * Rich Content in Action List Item
    * Info in Form

--- packages/dso-toolkit/src/components/form/content/static.content.ts
+++ packages/dso-toolkit/src/components/form/content/static.content.ts
@@ -7,6 +7,6 @@ export const staticContent: Omit<FormGroupStaticArgs, "infoButtonHandler" | "inf
   edit: false,
   infoButtonLabel: "Toelichting bij veld",
   infoActive: false,
-  infoText: '<div class="dso-rich-content"><p>Rich text</p></div>',
+  infoText: '<div class="dso-rich-content"><h5>Heading</h5><p>Rich text</p></div>',
   infoFixed: false,
 };
--- packages/dso-toolkit/src/components/form/content/textarea.content.ts
+++ packages/dso-toolkit/src/components/form/content/textarea.content.ts
@@ -9,6 +9,6 @@ export const textareaContent: Omit<FormGroupTextareaArgs, "infoButtonHandler" | 
   readonly: false,
   infoButtonLabel: "Toelichting bij veld",
   infoActive: false,
-  infoText: '<div class="dso-rich-content"><p>Rich text</p></div>',
+  infoText: '<div class="dso-rich-content"><h5>Heading</h5><p>Rich text</p></div>',
   infoFixed: false,
 };
