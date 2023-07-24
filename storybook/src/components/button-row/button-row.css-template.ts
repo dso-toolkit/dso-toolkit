d25a67e155fe937856d37212bb9ef1fe20e8758c
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2023-07-24 18:27:51 +0200

    #2234 Refactor: Geen string builder maar lit/classMap()

--- storybook/src/components/button-row/button-row.css-template.ts
+++ storybook/src/components/button-row/button-row.css-template.ts
@@ -1,13 +1,14 @@
 import { ButtonRow } from "dso-toolkit";
 import { html } from "lit-html";
 import { ComponentImplementation } from "../../templates";
+import { classMap } from "lit-html/directives/class-map.js";
 
 export const cssButtonRow: ComponentImplementation<ButtonRow> = {
   component: "buttonRow",
   implementation: "html-css",
   template: ({ buttonTemplate, anchorTemplate }) =>
     function buttonRowTemplate({ buttons, emphasized }) {
-      return html`<div class=${["dso-button-row", emphasized ? "dso-emphasized" : ""].join(" ")}>
+      return html`<div class="dso-button-row ${classMap({ "dso-emphasized": !!emphasized })}">
         ${buttons.map((button) => ("variant" in button ? buttonTemplate(button) : anchorTemplate(button)))}
       </div>`;
     },
