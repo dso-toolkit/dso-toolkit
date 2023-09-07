cfa4bfafc7c5dd401654313926fc174d358601c1
Luke Verlaan <luke-verlaan@hotmail.com>
2023-09-07 14:28:08 +0200

    #2184 [Task] Build: Custom property met toolkit versie

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 ### Tasks
 * Packages: Dependency updates ([#2241](https://github.com/dso-toolkit/dso-toolkit/issues/2241))
 * Storybook: Lit comments in HTML tab verwijderen ([#1987](https://github.com/dso-toolkit/dso-toolkit/issues/1987))
+* Build: Custom property met toolkit versie ([#2184](https://github.com/dso-toolkit/dso-toolkit/issues/2184))
 
 ## 58.1.0 - 28-08-2023
 
--- packages/dso-toolkit/gulp/build-styling.js
+++ packages/dso-toolkit/gulp/build-styling.js
@@ -1,20 +1,49 @@
-import postcss from "gulp-postcss";
 import gulp from "gulp";
-import gulpSass from "gulp-sass";
-import rename from "gulp-rename";
 import filter from "gulp-filter";
+import header from "gulp-header";
+import postcss from "gulp-postcss";
+import rename from "gulp-rename";
+import gulpSass from "gulp-sass";
 import * as dartSass from "sass";
 
 import { plugins } from "../postcss.config.js";
 
 const sass = gulpSass(dartSass);
 
+function getVersion() {
+  if (process.env.DT_VERSION) {
+    return process.env.DT_VERSION;
+  }
+
+  if (process.env.CI) {
+    if (typeof process.env.TRAVIS_TAG === "string" && process.env.TRAVIS_TAG[0] === "v") {
+      return process.env.TRAVIS_TAG.substring(1);
+    }
+
+    if (
+      typeof process.env.TRAVIS_BRANCH === "string" &&
+      (process.env.TRAVIS_BRANCH[0] === "#" || process.env.TRAVIS_BRANCH === "master")
+    ) {
+      return process.env.TRAVIS_BRANCH.replace(/#/, "_");
+    }
+  }
+
+  return undefined;
+}
+
 export function buildStyling() {
   const sassCompiler = sass().on("error", sass.logError);
+
+  const version = getVersion();
 
   return gulp
     .src("src/dso.scss", { sourcemaps: true })
     .pipe(sassCompiler)
+    .pipe(
+      header(
+        [`/* DSO Toolkit version: ${version} */`, `:root { --dso-toolkit-version: ${version} }`, "", ""].join("\n")
+      )
+    )
     .pipe(gulp.dest("dist", { sourcemaps: "." }))
     .pipe(filter("dso.css"))
     .pipe(postcss(plugins))
