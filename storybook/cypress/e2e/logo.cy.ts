fe454201f39d1c06e0c4ae7c1c679143a97c958a
Anneke Sinnema <mail@annekesinnema.nl>
2023-10-16 21:00:54 +0200

    #2265 Update tests
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- storybook/cypress/e2e/logo.cy.ts
+++ storybook/cypress/e2e/logo.cy.ts
@@ -5,21 +5,36 @@ describe("Logo", () => {
 
   it("shows a label", () => {
     cy.get("dso-logo").invoke("attr", "label", "Beheerportaal").shadow().find(".logo-label").should("be.visible");
+    cy.percySnapshot();
+    cy.injectAxe();
+    cy.checkA11y("dso-logo");
   });
 
   it("only shows a label on smaller screens", () => {
     cy.viewport(400, 720);
+    cy.percySnapshot();
     cy.get("dso-logo")
       .invoke("attr", "label", "Beheerportaal")
       .shadow()
       .find(".logo-label")
       .should("be.visible")
+      .should("have.text", "Beheerportaal")
       .find(".logo-wordmark")
       .should("not.exist");
+    cy.injectAxe();
+    cy.checkA11y("dso-logo");
   });
 
   it("shows the beta tag", () => {
-    cy.get("dso-logo").invoke("attr", "ribbon", "beta").shadow().find(".logo-ribbon").should("be.visible");
+    cy.get("dso-logo")
+      .invoke("attr", "ribbon", "beta")
+      .shadow()
+      .find(".logo-ribbon")
+      .should("be.visible")
+      .should("have.text", "beta");
+    cy.percySnapshot();
+    cy.injectAxe();
+    cy.checkA11y("dso-logo");
   });
 
   it("should be accessible", () => {
