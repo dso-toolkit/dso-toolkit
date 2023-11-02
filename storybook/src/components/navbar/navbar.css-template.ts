acd1ab2faf5e7cd93a3a22e612ed3565dc0347c4
Hans Grimm <hans@grimm.nl>
2023-11-02 15:38:01 +0100

    #2387 [Added] Zoekoverlay: "Nav Bar" uitbreiding "extension" en nieuw component "Mark Bar"

--- storybook/src/components/navbar/navbar.css-template.ts
+++ storybook/src/components/navbar/navbar.css-template.ts
@@ -40,6 +40,44 @@ export const cssNavbar: ComponentImplementation<Navbar> = {
             )}
           </ul>
         </nav>
+        <!-- start dso-document-search markup -->
+        <div class="dso-document-search">
+          <button
+            type="button"
+            class="dso-expand-button"
+            aria-controls="expand-container"
+            onClick="javascript: this.classList.toggle('active');this.toggleAttribute('aria-expanded');"
+          >
+            <!-- ^^ aria-expanded not set correctly. to be fixed -->
+            <!-- js on button to be replaced :) -->
+            <span class="text-start"><dso-icon icon="search" class="hydrated"></dso-icon>Zoeken</span>
+            <span class="text-stop"><dso-icon icon="times" class="hydrated"></dso-icon>Stoppen</span>
+          </button>
+          <div class="dso-expander" id="expand-container">
+            <div class="dso-expand-container">
+              <div class="dso-search-bar">
+                <div class="dso-search-bar-input">
+                  <label for="search-bar-id" class="sr-only">Zoeken in document</label>
+                  <span class="dso-search-icon" aria-hidden="true"></span>
+                  <input type="text" id="search-bar-id" />
+                  <button type="button">Zoekopdracht legen</button>
+                </div>
+                <div class="dso-button-container">
+                  <button type="button" disabled>
+                    <dso-icon icon="chevron-up" class="hydrated"></dso-icon
+                    ><span class="sr-only">vorig zoekresultaat</span>
+                  </button>
+                  <span>1/8</span>
+                  <button type="button">
+                    <dso-icon icon="chevron-down" class="hydrated"></dso-icon
+                    ><span class="sr-only">volgend zoekresultaat</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end dso-document-search markup -->
       `;
     },
 };
