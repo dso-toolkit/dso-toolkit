5ed6b37de4452f0664d718dffea313edbd124f46
Eric Tamminga <eric@iterox.com>
2024-01-04 19:42:34 +0100

    #2489 [Fixed] Logo: Responsiveness stuk

--- storybook/cypress/e2e/logo.cy.ts
+++ storybook/cypress/e2e/logo.cy.ts
@@ -3,24 +3,46 @@ describe("Logo", () => {
     cy.visit("http://localhost:45000/iframe.html?id=core-logo--default");
   });
 
-  it("shows a label", () => {
-    cy.get("dso-logo").invoke("attr", "label", "Beheerportaal").shadow().find(".logo-label").should("be.visible");
+  it("not shows a label", () => {
+    cy.get("dso-logo")
+      .should("not.have.attr", "label", "Beheerportaal")
+      .shadow()
+      .find(".logo-label")
+      .should("not.exist");
+    cy.get("dso-logo").shadow().find(".logo-wordmark").should("exist");
     cy.percySnapshot();
     cy.injectAxe();
     cy.checkA11y("dso-logo");
   });
 
-  it("only shows a label on smaller screens", () => {
-    cy.viewport(400, 720);
-    cy.percySnapshot();
+  it("shows a label and logo-wordmark on screens wider than 767px", () => {
     cy.get("dso-logo")
-      .invoke("attr", "label", "Beheerportaal")
+      .invoke("prop", "label", "Beheerportaal")
+      .should("have.attr", "label", "Beheerportaal")
+      .shadow()
+      .find(".logo-label")
+      .should("be.visible");
+    cy.get("dso-logo").invoke("prop", "label", "Beheerportaal").shadow().find(".logo-wordmark").should("be.visible");
+    cy.percySnapshot();
+    cy.injectAxe();
+    cy.checkA11y("dso-logo");
+  });
+
+  it("shows a label and no logo-wordmark on screens smaller than 768px", () => {
+    cy.viewport(400, 720);
+    cy.get("dso-logo")
+      .invoke("prop", "label", "Beheerportaal")
+      .should("have.attr", "label", "Beheerportaal")
       .shadow()
       .find(".logo-label")
       .should("be.visible")
-      .should("have.text", "Beheerportaal")
+      .should("have.text", "Beheerportaal");
+    cy.get("dso-logo")
+      .invoke("prop", "label", "Beheerportaal")
+      .shadow()
       .find(".logo-wordmark")
-      .should("not.exist");
+      .should("not.be.visible");
+    cy.percySnapshot();
     cy.injectAxe();
     cy.checkA11y("dso-logo");
   });
@@ -39,7 +61,6 @@ describe("Logo", () => {
 
   it("should be accessible", () => {
     cy.percySnapshot();
-
     cy.injectAxe();
     cy.checkA11y("dso-logo");
   });
