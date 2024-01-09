9d28cafc056c45cca46467d9a282c77e68795625
Eric Tamminga <eric@iterox.com>
2024-01-09 14:10:11 +0100

    #2294 storybook angular voor ListButton gefixt mbt NG0302: The pipe 'trustHtml' could not be found in the 'StorybookWrapperComponent' component.

--- angular-workspace/components/list-button/list-button.stories.ts
+++ angular-workspace/components/list-button/list-button.stories.ts
@@ -1,14 +1,22 @@
-import { storiesOf } from "@storybook/angular";
+import { moduleMetadata, storiesOf } from "@storybook/angular";
 import { storiesOfListButton } from "dso-toolkit";
 
 import { templateContainer } from "../../templates";
 import readme from "./readme.md?raw";
+import { TrustHtmlPipe } from "../trust-html.pipe";
 
 storiesOfListButton({
   parameters: {
     module,
     storiesOf,
     readme,
+    storyApiOptions: {
+      decorators: [
+        moduleMetadata({
+          declarations: [TrustHtmlPipe],
+        }),
+      ],
+    },
   },
   templateContainer,
   storyTemplates: ({ listButtonTemplate }) => ({ listButtonTemplate }),
