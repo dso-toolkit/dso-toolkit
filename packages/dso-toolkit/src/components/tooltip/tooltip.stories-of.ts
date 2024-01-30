07194cc5eee32acfb3189e2d6e383a4de063ca31
Dion Verhelst <dion.verhelst@infoprojects.nl>
2024-01-30 14:11:56 +0100

    #2316 [Task] Storybook: storiesOf naar CSF
    * Tooltip component

--- packages/dso-toolkit/src/components/tooltip/tooltip.stories-of.ts
+++ packages/dso-toolkit/src/components/tooltip/tooltip.stories-of.ts
@@ -1,55 +1,69 @@
+import { ComponentAnnotations, Renderer } from "@storybook/types";
+
 import { HandlerFunction } from "@storybook/addon-actions";
 import { v4 as uuidv4 } from "uuid";
 
 import { TooltipArgs, tooltipArgsMapper, tooltipArgTypes } from "./tooltip.args.js";
 import { Tooltip } from "./tooltip.models.js";
-import { StoriesOfArguments, storiesOfFactory } from "../../storybook/index.js";
 
-export interface TooltipTemplates<TemplateFnReturnType> {
+import { StoriesParameters, StoryObj } from "../../template-container.js";
+import { compiler } from "markdown-to-jsx";
+import { MetaOptions } from "../../storybook/meta-options.interface.js";
+
+type TooltipStory = StoryObj<TooltipArgs, Renderer>;
+
+interface TooltipStories {
+  AsChild: TooltipStory;
+  AsSibling: TooltipStory;
+}
+
+interface TooltipStoriesParameters<Implementation, Templates, TemplateFnReturnType>
+  extends StoriesParameters<Implementation, Templates, TemplateFnReturnType, TooltipTemplates<TemplateFnReturnType>> {}
+
+interface TooltipTemplates<TemplateFnReturnType> {
   tooltipTemplate: (tooltipProperties: Tooltip) => TemplateFnReturnType;
   asChildTemplate: (tooltip: TemplateFnReturnType, id: string, action: HandlerFunction) => TemplateFnReturnType;
   asSiblingTemplate: (tooltip: TemplateFnReturnType, id: string, action: HandlerFunction) => TemplateFnReturnType;
 }
 
-export function storiesOfTooltip<Implementation, Templates, TemplateFnReturnType>(
-  storiesOfArguments: StoriesOfArguments<
-    Implementation,
-    Templates,
-    TemplateFnReturnType,
-    TooltipTemplates<TemplateFnReturnType>
-  >,
-) {
-  return storiesOfFactory("Tooltip", storiesOfArguments, (stories, templateMapper) => {
-    stories.addParameters({
-      argTypes: tooltipArgTypes,
-    });
+export function tooltipMeta<TRenderer extends Renderer>({ readme }: MetaOptions = {}): ComponentAnnotations<
+  TRenderer,
+  TooltipArgs
+> {
+  return {
+    argTypes: tooltipArgTypes,
+    parameters: {
+      docs: readme
+        ? {
+            page: () => compiler(readme),
+          }
+        : {},
+    },
+  };
+}
 
-    stories.add(
-      "as child",
-      templateMapper<TooltipArgs>((args, { tooltipTemplate, asChildTemplate }) =>
+export function tooltipStories<Implementation, Templates, TemplateFnReturnType>({
+  storyTemplates,
+  templateContainer,
+}: TooltipStoriesParameters<Implementation, Templates, TemplateFnReturnType>): TooltipStories {
+  return {
+    AsChild: {
+      args: {
+        id: uuidv4(),
+        position: "right",
+      },
+      render: templateContainer.render(storyTemplates, (args, { tooltipTemplate, asChildTemplate }) =>
         asChildTemplate(tooltipTemplate(tooltipArgsMapper(args)), args.id, args.action),
       ),
-      {
-        args: {
-          id: uuidv4(),
-          position: "right",
-        },
+    },
+    AsSibling: {
+      args: {
+        id: uuidv4(),
+        position: "bottom",
       },
-    );
-
-    stories.add(
-      "as sibling",
-      templateMapper<TooltipArgs>((args, { tooltipTemplate, asSiblingTemplate }) =>
+      render: templateContainer.render(storyTemplates, (args, { tooltipTemplate, asSiblingTemplate }) =>
         asSiblingTemplate(tooltipTemplate(tooltipArgsMapper(args)), args.id, args.action),
       ),
-      {
-        args: {
-          id: uuidv4(),
-          position: "bottom",
-        },
-      },
-    );
-
-    return stories;
-  });
+    },
+  };
 }
