9e3b15c0be7d0bdc16c97f73c1290f4099b149fb
Dion Verhelst <dion.verhelst@infoprojects.nl>
2024-02-05 14:25:59 +0100

    #2316 [Task] Storybook: storiesOf naar CSF
    * Highlight Box component

---
+++ storybook/src/components/highlight-box/highlight-box.core-stories.ts
@@ -0,0 +1,65 @@
+import type { Meta } from "@storybook/web-components";
+import { HighlightBoxArgs, highlightBoxMeta, highlightBoxStories } from "dso-toolkit";
+
+import { templateContainer } from "../../templates";
+import { html } from "lit-html";
+
+import readme from "@dso-toolkit/core/src/components/highlight-box/readme.md?raw";
+
+const meta: Meta<HighlightBoxArgs> = {
+  ...highlightBoxMeta({ readme }),
+  title: "Core/Highlight Box",
+};
+
+export default meta;
+
+const { Default, Yellow, WhiteWithDropshadow, WithBorder, WithIcon, WithBannerImage } = highlightBoxStories({
+  templateContainer,
+  storyTemplates: (templates) => {
+    const { highlightBoxTemplate, anchorTemplate, buttonTemplate, richContentTemplate } = templates;
+
+    return {
+      highlightBoxTemplate,
+      content: richContentTemplate({
+        children: html`
+          <h3>Toelichting: Vergunningvrij onder voorbehoud</h3>
+          <p>
+            Het Informatiehuis Bouw kent, op basis van de huidige gebruikerswensen, vier Informatieproducten, namelijk
+            het Opleverdossier, de Bouwregelgeving, de Vergunningvrije bouwwerken en een Digitaliseringshulp.
+          </p>
+          <p>
+            Het Opleverdossier (zie voor een nadere omschrijving van de informatieproducten de volgende paragrafen) is
+            de, door gebruikers gewenste, centrale registratie waarin alle informatie over een bouwwerk is opgenomen.
+            Het gaat hierbij om de tekeningen, berekeningen en de resultaten van de kwaliteitsborging (zoals toetsen en
+            inspecties).
+          </p>
+          <p>
+            De ${anchorTemplate({ label: "Bouwregelgeving", url: "#", mode: "download" })} is een database met alle
+            bouwregelgeving in Nederland, die op zodanige wijze moet zijn ingericht en ontsloten dat die voldoet aan de
+            eisen van de Omgevingswet (3B's), en daarmee bruikbaar is in de ontwerp- en toetsingsfase van ieder
+            bouwwerk.
+          </p>
+          <p>
+            Het derde informatieproduct zijn de Vergunningvrije bouwwerken, hierin zijn opgenomen de (bekende)
+            bouwwerken die vergunningvrij, maar niet regelvrij, zijn gerealiseerd. Het vierde en vooralsnog laatste
+            informatieproduct is de Digitaliseringshulp, een service voor het centraal en gestandaardiseerd
+            digitaliseren van documenten.
+          </p>
+          <div class="dso-button-row">
+            ${buttonTemplate({ variant: "primary", label: "Primaire button", url: "#" })}
+            ${buttonTemplate({ variant: "secondary", label: "Secundaire button", url: "#" })}
+            ${buttonTemplate({
+              variant: "tertiary",
+              label: "Tertiare button",
+              icon: { icon: "chevron-down" },
+              iconMode: "after",
+              url: "#",
+            })}
+          </div>
+        `,
+      }),
+    };
+  },
+});
+
+export { Default, Yellow, WhiteWithDropshadow, WithBorder, WithIcon, WithBannerImage };
