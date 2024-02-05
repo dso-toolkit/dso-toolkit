0628aee41a2d404bfb5da52868292811baa4a74e
Dion Verhelst <dion.verhelst@infoprojects.nl>
2024-02-05 15:18:29 +0100

    #2316 [Task] Storybook: storiesOf naar CSF
    * Highlight Box React component
    * Header React component
    * Helpcenter Panel React component

--- packages/react/src/components/header/header.stories.ts
+++ packages/react/src/components/header/header.stories.ts
@@ -1,15 +1,27 @@
-import { storiesOfHeader } from "dso-toolkit";
-import { storiesOf } from "@storybook/react";
+import type { Meta } from "@storybook/web-components";
+import { HeaderArgs, headerMeta, headerStories } from "dso-toolkit";
+
 import { templateContainer } from "../../templates";
 
 import readme from "./readme.md?raw";
 
-storiesOfHeader({
-  parameters: {
-    module,
-    storiesOf,
-    readme,
-  },
-  templateContainer,
-  storyTemplates: ({ headerTemplate }) => ({ headerTemplate }),
-});
+const meta: Meta<HeaderArgs> = {
+  ...headerMeta({ readme }),
+  title: "Header",
+};
+
+export default meta;
+
+const { Default, WithLabel, WithRibbon, WithLabelAndRibbon, UserHomeActive, WithLinkToHelp, WithButtonToHelp } =
+  headerStories({
+    templateContainer,
+    storyTemplates: (templates) => {
+      const { headerTemplate } = templates;
+
+      return {
+        headerTemplate,
+      };
+    },
+  });
+
+export { Default, WithLabel, WithRibbon, WithLabelAndRibbon, UserHomeActive, WithLinkToHelp, WithButtonToHelp };
