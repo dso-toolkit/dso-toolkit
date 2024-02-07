d7a3111a5185b9f4ce527805432aa01220398b0d
Dion Verhelst <dion.verhelst@infoprojects.nl>
2024-02-07 13:31:55 +0100

    #2316 [Task] Storybook: storiesOf naar CSF
    * Highlight Box Angular component
    * Header Angular component
    * Helpcenter Panel Angular component

--- angular-workspace/components/helpcenter-panel/helpcenter-panel.stories.ts
+++ angular-workspace/components/helpcenter-panel/helpcenter-panel.stories.ts
@@ -1,24 +1,57 @@
-import { storiesOf } from "@storybook/angular";
+// import { storiesOf } from "@storybook/angular";
 
-import { storiesOfHelpcenterPanel } from "dso-toolkit";
+// import { storiesOfHelpcenterPanel } from "dso-toolkit";
+// import { templateContainer } from "../../templates";
+
+// import readme from "./readme.md?raw";
+
+import type { Meta } from "@storybook/web-components";
+
+import { HelpcenterPanelArgs, helpcenterPanelMeta, helpcenterPanelStories } from "dso-toolkit";
 import { DsoHelpcenterPanel } from "../../projects/component-library/src/public-api";
+
 import { templateContainer } from "../../templates";
 
 import readme from "./readme.md?raw";
 
-storiesOfHelpcenterPanel({
-  parameters: {
-    module,
-    storiesOf,
-    readme,
-    storyApiOptions: {
-      parameters: [
-        {
-          component: DsoHelpcenterPanel,
-        },
-      ],
+const meta: Meta<HelpcenterPanelArgs> = {
+  ...helpcenterPanelMeta({ readme }),
+  title: "Helpcenter Panel",
+  parameters: [
+    {
+      component: DsoHelpcenterPanel,
     },
-  },
+  ],
+};
+
+export default meta;
+
+const { HelpcenterPanel } = helpcenterPanelStories({
   templateContainer,
-  storyTemplates: ({ helpcenterPanelTemplate }) => ({ helpcenterPanelTemplate }),
+  storyTemplates: (templates) => {
+    const { helpcenterPanelTemplate } = templates;
+
+    return {
+      helpcenterPanelTemplate,
+    };
+  },
 });
+
+export { HelpcenterPanel };
+
+// storiesOfHelpcenterPanel({
+//   parameters: {
+//     module,
+//     storiesOf,
+//     readme,
+//     storyApiOptions: {
+//       parameters: [
+//         {
+//           component: DsoHelpcenterPanel,
+//         },
+//       ],
+//     },
+//   },
+//   templateContainer,
+//   storyTemplates: ({ helpcenterPanelTemplate }) => ({ helpcenterPanelTemplate }),
+// });
