9183ae04766ecdfe5c88fbc7a8cc0081e3612f5a
Wesley Hendriks <wesleyhendriks@protonmail.ch>
2024-02-26 15:25:54 +0100

    #2525 [Changed] Improve list elements

--- packages/core/src/components/advanced-select/advanced-select.tsx
+++ packages/core/src/components/advanced-select/advanced-select.tsx
@@ -94,13 +94,13 @@ export class AdvancedSelect implements ComponentInterface {
           </span>
         </button>
         <div class={{ "groups-container": true, open: this.open }}>
-          <ul class="groups">
+          <ul class="groups" role="list">
             {this.options.map((optionsOrGroup) => {
               if ("options" in optionsOrGroup) {
                 return (
                   <li class={{ group: true, [`group-${optionsOrGroup.variant}`]: !!optionsOrGroup.variant }}>
                     {optionsOrGroup.label && <p class="group-label">{optionsOrGroup.label}</p>}
-                    <ul class="options">
+                    <ul class="options" role="list">
                       {optionsOrGroup.options.map((option) => (
                         <OptionElement
                           option={option}
