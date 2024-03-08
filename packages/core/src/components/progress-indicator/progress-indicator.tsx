3976e32eca81155b9970b8f5d9e7ce575e8055a1
Eric Tamminga <eric@iterox.com>
2024-03-08 11:12:50 +0100

    #2573 obsolete comment removed

--- packages/core/src/components/progress-indicator/progress-indicator.tsx
+++ packages/core/src/components/progress-indicator/progress-indicator.tsx
@@ -30,7 +30,6 @@ export class Progressindicator {
     return (
       <Host>
         <span class="dso-progress-indicator-spinner" role="progressbar" aria-labelledby="progress-indicator-label">
-          {/* Keep in sync with /packages/css/src/components/progress-indicator/progress-indicator.scss */}
           <dso-icon icon="spinner"></dso-icon>
         </span>
         <span id="progress-indicator-label" class="dso-progress-indicator-label">
