45b214cdd8c908ddd1de8d48a1c111b57152e9cd
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2024-05-26 20:04:10 +0200

    #2604 Geen /-suffix bij single file copy

--- deploy.sh
+++ deploy.sh
@@ -33,6 +33,6 @@ fi
 
 yarn tsx ./scripts/list-versions-azure-blob-storage.ts > versions.json
 cat versions.json
-azcopy copy --overwrite=true --from-to=LocalBlob ./versions.json "https://${DT_AZURE_STORAGE_HOST}/${DT_AZURE_STORAGE_CONTAINER}/dso-toolkit.nl/www/versions.json/?${SAS_TOKEN}"
+azcopy copy --overwrite=true --from-to=LocalBlob ./versions.json "https://${DT_AZURE_STORAGE_HOST}/${DT_AZURE_STORAGE_CONTAINER}/dso-toolkit.nl/www/versions.json?${SAS_TOKEN}"
 
 # Todo: Prune old versions
