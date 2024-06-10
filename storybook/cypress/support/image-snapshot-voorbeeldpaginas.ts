d7acb6d7ea378c1f2cf43078439f3271b8b23289
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2024-06-10 22:13:31 +0200

    #2588 skip voorbeeldpagina screenshots

--- storybook/cypress/support/image-snapshot-voorbeeldpaginas.ts
+++ storybook/cypress/support/image-snapshot-voorbeeldpaginas.ts
@@ -1,5 +1,5 @@
 export function voorbeeldpaginaImageSnapshots(label: string, voorbeeldpaginas: string[], args?: string) {
-  describe(`Voorbeeldpagina image snapshots (${label})`, () => {
+  describe.skip(`Voorbeeldpagina image snapshots (${label})`, () => {
     for (const id of voorbeeldpaginas) {
       it(`matches image snapshot of ${id} (${label})`, () => {
         cy.visit(`http://localhost:45000/iframe.html?id=${id}${args ? `&args=${args}` : ""}`);
