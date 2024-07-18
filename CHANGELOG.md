107bde2b045c35f2b4c82f63226c463edbb81239
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2024-07-18 10:57:40 +0200

    #1337 [Task] Build: Release automatisering

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 
 ## 🥌 Release 62.29.0 - 17-07-2024
 
+### Tasks
+* Build: Release automatisering ([#1337](https://github.com/dso-toolkit/dso-toolkit/issues/1337))
+
 ### Changed
 * Button Secondary: Hover and active state ([#2699](https://github.com/dso-toolkit/dso-toolkit/issues/2699))
 
---
+++ scripts/release/index.ts
@@ -0,0 +1,77 @@
+import * as prettier from "prettier";
+import { readFile, writeFile } from "fs/promises";
+import minimist from "minimist";
+
+interface Args {
+  version: string | undefined;
+  emoji: string | undefined;
+}
+
+const args = minimist<Args>(process.argv.slice(2));
+
+main(args.version, args.emoji).catch((error) => {
+  console.error(error);
+
+  process.exit(1);
+});
+
+async function main(version: string | undefined, emoji: string | undefined) {
+  if (!version) {
+    throw new Error("No version provided");
+  }
+
+  await updatePackageJsons(version);
+  await updateChangelog(version, emoji);
+}
+
+async function updatePackageJsons(version: string) {
+  const packagePaths = [
+    "packages/dso-toolkit",
+    "packages/core",
+    "packages/react",
+    "angular-workspace/projects/component-library",
+  ];
+
+  for (const packagePath of packagePaths) {
+    const packageJson = JSON.parse(await readFile(`${packagePath}/package.json`, "utf-8"));
+
+    packageJson.version = version;
+
+    await writeFile(
+      `${packagePath}/package.json`,
+      await format(JSON.stringify(packageJson, null, 2), `${packagePath}/package.json`),
+    );
+  }
+}
+
+async function updateChangelog(version: string, emoji: string | undefined) {
+  if (!emoji) {
+    throw new Error("No emoji provided");
+  }
+
+  const changelog = await readFile("CHANGELOG.md", "utf-8");
+
+  const lines = changelog.split("\n");
+
+  const releasePlaceholderIndex = lines.findIndex((line) => line.startsWith("## Next"));
+
+  if (releasePlaceholderIndex === -1) {
+    throw new Error("No release placeholder found");
+  }
+
+  lines.splice(
+    releasePlaceholderIndex + 2,
+    0,
+    `## ${emoji} Release ${version} - ${new Date().toISOString().slice(0, 10)}`,
+    "",
+  );
+
+  await writeFile("CHANGELOG.md", lines.join("\n"));
+}
+
+async function format(content: string, filepath: string) {
+  return prettier.format(content, {
+    ...(await prettier.resolveConfig(filepath)),
+    filepath,
+  });
+}
