e434ecdbf98bbf6d18abe0d1beb78c8b0e4901a1
Eric Tamminga <eric@iterox.com>
2024-10-17 08:35:41 +0200

    #2768 Review

--- scripts/release/index.ts
+++ scripts/release/index.ts
@@ -38,7 +38,10 @@ async function updatePackageJsons(version: string) {
     packageJson.version = version;
 
     if (packageJson.peerDependencies?.["@dso-toolkit/core"]) {
-      packageJson.peerDependencies["@dso-toolkit/core"] = `${version}`;
+      packageJson.peerDependencies["@dso-toolkit/core"] = version;
+    }
+    if (packageJson.peerDependencies?.["dso-toolkit"]) {
+      packageJson.peerDependencies["dso-toolkit"] = version;
     }
 
     await writeFile(
