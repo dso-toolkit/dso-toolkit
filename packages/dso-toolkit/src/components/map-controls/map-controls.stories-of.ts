78327cfb11d61aba46f4e35155b321d842db4b17
Eric Tamminga <eric@iterox.com>
2024-10-18 14:29:39 +0200

    #2316 MapControls

--- packages/dso-toolkit/src/components/map-controls/map-controls.stories-of.ts
+++ packages/dso-toolkit/src/components/map-controls/map-controls.stories-of.ts
@@ -1,50 +1,70 @@
-import { Addon_DecoratorFunction } from "@storybook/types";
-import { StoriesOfArguments, storiesOfFactory } from "../../storybook/index.js";
+import { ComponentAnnotations, PartialStoryFn, Renderer } from "@storybook/types";
 
 import { mapControlsArgsMapper, mapControlsArgTypes, MapControlsArgs } from "./map-controls.args.js";
-import { baseLayers, overlays } from "./map-controls.content.js";
 import { MapControls } from "./map-controls.models.js";
+import { baseLayers, overlays } from "./map-controls.content.js";
+import { mapControlsDemoCss } from "./map-controls.demo";
+
+import { StoriesParameters, StoryObj } from "../../template-container.js";
+import { MetaOptions } from "../../storybook/meta-options.interface";
+import { compiler } from "markdown-to-jsx";
+
+export type MapControlsDecorator<TemplateFnReturnType> = (story: PartialStoryFn, css: string) => TemplateFnReturnType;
+
+interface MapControlsStories {
+  MapControls: StoryObj<MapControlsArgs, Renderer>;
+}
+
+interface MapControlsStoriesParameters<Implementation, Templates, TemplateFnReturnType>
+  extends StoriesParameters<
+    Implementation,
+    Templates,
+    TemplateFnReturnType,
+    MapControlsTemplates<TemplateFnReturnType>
+  > {
+  decorator: MapControlsDecorator<TemplateFnReturnType>;
+}
 
 export interface MapControlsTemplates<TemplateFnReturnType> {
   mapControlsTemplate: (mapControlsProperties: MapControls) => TemplateFnReturnType;
 }
 
-export interface MapControlsParameters<TemplateFnReturnType> {
-  decorator: Addon_DecoratorFunction<TemplateFnReturnType>;
+export function mapControlsMeta<TRenderer extends Renderer>({ readme }: MetaOptions = {}): ComponentAnnotations<
+  TRenderer,
+  MapControlsArgs
+> {
+  return {
+    argTypes: mapControlsArgTypes,
+    args: {
+      open: false,
+      baseLayers,
+      overlays,
+    },
+    parameters: {
+      html: {
+        root: "#map-container-mock",
+      },
+      layout: "fullscreen",
+      docs: readme
+        ? {
+            page: () => compiler(readme),
+          }
+        : {},
+    },
+  };
 }
 
-export function storiesOfMapControls<Implementation, Templates, TemplateFnReturnType>(
-  storiesOfArguments: StoriesOfArguments<
-    Implementation,
-    Templates,
-    TemplateFnReturnType,
-    MapControlsTemplates<TemplateFnReturnType>
-  >,
-  { decorator }: MapControlsParameters<TemplateFnReturnType>,
-) {
-  return storiesOfFactory("Map Controls", storiesOfArguments, (stories, templateMapper) => {
-    stories
-      .addParameters({
-        argTypes: mapControlsArgTypes,
-        args: {
-          open: false,
-          baseLayers,
-          overlays,
-        },
-        html: {
-          root: "#map-container-mock",
-        },
-        layout: "fullscreen",
-      })
-      .addDecorator(decorator);
-
-    stories.add(
-      "Map Controls",
-      templateMapper<MapControlsArgs>((args, { mapControlsTemplate }) =>
+export function mapControlsStories<Implementation, Templates, TemplateFnReturnType>({
+  storyTemplates,
+  templateContainer,
+  decorator,
+}: MapControlsStoriesParameters<Implementation, Templates, TemplateFnReturnType>): MapControlsStories {
+  return {
+    MapControls: {
+      decorators: [(story) => decorator(story, mapControlsDemoCss)],
+      render: templateContainer.render(storyTemplates, (args, { mapControlsTemplate }) =>
         mapControlsTemplate(mapControlsArgsMapper(args)),
       ),
-    );
-
-    return stories;
-  });
+    },
+  };
 }
---
+++ storybook/src/components/map-controls/map-controls.core-stories.ts
@@ -0,0 +1,30 @@
+import type { Meta } from "@storybook/web-components";
+
+import { MapControlsArgs, mapControlsMeta, mapControlsStories } from "dso-toolkit";
+
+import readme from "@dso-toolkit/core/src/components/map-controls/readme.md?raw";
+
+import { templateContainer } from "../../templates";
+
+import { decorator } from "./map-controls.decorator";
+
+const meta: Meta<MapControlsArgs> = {
+  ...mapControlsMeta({ readme }),
+  title: "Core/Map Controls",
+};
+
+export default meta;
+
+const { MapControls } = mapControlsStories({
+  templateContainer,
+  storyTemplates: (templates) => {
+    const { mapControlsTemplate } = templates;
+
+    return {
+      mapControlsTemplate,
+    };
+  },
+  decorator,
+});
+
+export { MapControls };
