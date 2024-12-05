b9867847a274f3fe2c262409c780d37225d597ee
Eric Tamminga <eric@iterox.com>
2024-12-05 10:07:58 +0100

    #2904 [Task] Packages: Dependencies updates

--- packages/react/src/components/expandable/expandable.decorator.tsx
+++ packages/react/src/components/expandable/expandable.decorator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { ExpandableDecorator } from "dso-toolkit";
 
 export const decorator: ExpandableDecorator<JSX.Element> = (story) => (
