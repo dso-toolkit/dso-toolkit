85d2be871a28fe7d25da6a2e0ef0e40a9805d86f
Eric Tamminga <eric@iterox.com>
2024-12-05 10:07:58 +0100

    #2904 [Task] Packages: Dependencies updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 
 ## Next
 
+### Task
+* Packages: Dependencies updates ([#2904](https://github.com/dso-toolkit/dso-toolkit/issues/2904))
+
 ## 👹 Release 67.2.0 - 2024-12-19
 
 ### Changed
--- packages/react/src/components/scrollable/scrollable.decorator.tsx
+++ packages/react/src/components/scrollable/scrollable.decorator.tsx
@@ -1,8 +1,8 @@
-import { Addon_DecoratorFunction } from "@storybook/types";
 import React from "react";
+import { ScrollableDecorator } from "dso-toolkit";
 
-export const decorator: Addon_DecoratorFunction<JSX.Element> = (story) => (
+export const decorator: ScrollableDecorator<JSX.Element> = (story) => (
   <div id="scrollable-mock" style={{ backgroundColor: "#efefef", height: "750px", width: "500px" }}>
-    {story()}
+    {story() as React.ReactNode}
   </div>
 );
