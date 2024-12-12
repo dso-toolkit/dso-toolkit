dce6e6871f871567df1a00ccd1e951dcde03d7f0
cvanderleij <cvanderleij@ilionx.com>
2024-12-12 09:11:32 +0100

    #2692 [Task] Responsive-element: e2e test schrijven - review feedback

--- storybook/cypress/e2e/responsive-element.cy.ts
+++ storybook/cypress/e2e/responsive-element.cy.ts
@@ -12,17 +12,22 @@ describe("Responsive element", () => {
   it("should have size (small, medium, large) attribute depending on the viewport", () => {
     cy.viewport(1312, 660);
     cy.get(responsiveElementSelector).first().should("have.attr", "large");
+    cy.matchImageSnapshot(`${Cypress.currentTest.title}: 1312 - large`);
 
     cy.viewport(992, 660);
     cy.get(responsiveElementSelector).first().should("have.attr", "medium");
+    cy.matchImageSnapshot(`${Cypress.currentTest.title}: 992 - medium`);
 
     cy.viewport(656, 660);
     cy.get(responsiveElementSelector).first().should("have.attr", "large");
+    cy.matchImageSnapshot(`${Cypress.currentTest.title}: 656 - large`);
 
     cy.viewport(407, 660);
     cy.get(responsiveElementSelector).first().should("have.attr", "medium");
+    cy.matchImageSnapshot(`${Cypress.currentTest.title}: 407 - medium`);
 
     cy.viewport(406, 660);
     cy.get(responsiveElementSelector).first().should("have.attr", "small");
+    cy.matchImageSnapshot(`${Cypress.currentTest.title}: 406 - small`);
   });
 });
