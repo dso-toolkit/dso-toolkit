24bb29eb702dd9031585e44188c94b128d39610f
Chris van der Leij <cvanderleij@ilionx.com>
2025-01-09 10:58:09 +0100

    #2844 [Changed] Icon Mixins: Grote spritesheet zorgt voor CPU usage
    * #2844 [Changed] Icon Mixins: Grote spritesheet zorgt voor CPU usage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 
 ## Next
 
+### Changed
+* Icon Mixins: Grote spritesheet zorgt voor CPU usage ([#2844](https://github.com/dso-toolkit/dso-toolkit/issues/2844))
+
 ### Task
 * Packages: Dependencies updates ([#2904](https://github.com/dso-toolkit/dso-toolkit/issues/2904))
 
--- packages/dso-toolkit/gulpfile.js
+++ packages/dso-toolkit/gulpfile.js
@@ -2,13 +2,13 @@ import gulp from "gulp";
 
 import { buildScripting } from "./gulp/build-scripting.js";
 import { buildStyling } from "./gulp/build-styling.js";
-import { buildSvgSpritesheet } from "./gulp/build-svg-spritesheet.js";
+import { buildSvgSpritesheets } from "./gulp/build-svg-spritesheets.js";
 import { cleanDist } from "./gulp/clean-dist.js";
 import { copyMiscellaneous } from "./gulp/copy-miscellaneous.js";
 import { watcher } from "./gulp/watcher.js";
 
 gulp.task(
   "build",
-  gulp.series(cleanDist, gulp.parallel(copyMiscellaneous, buildScripting, buildStyling, buildSvgSpritesheet)),
+  gulp.series(cleanDist, gulp.parallel(copyMiscellaneous, buildScripting, buildStyling, buildSvgSpritesheets)),
 );
 gulp.task("default", gulp.series("build", watcher));
