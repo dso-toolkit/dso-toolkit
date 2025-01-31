28ffc5f7f5ad40f4ef13f2294878a863dda142de
cvanderleij <cvanderleij@ilionx.com>
2025-01-31 11:21:07 +0100

    #2142 [Added] Contactinformatie: Nieuw component: PR feedback v5

--- storybook/src/components/action-list/action-list.content.ts
+++ storybook/src/components/action-list/action-list.content.ts
@@ -2,6 +2,7 @@ import { ActionListItem } from "dso-toolkit";
 import { TemplateResult, html } from "lit-html";
 
 import { Templates } from "../../templates";
+import { content as contactInformationContent } from "dso-toolkit/dist/components/contact-information/contact-information.content";
 
 function item1({ buttonTemplate }: Templates): ActionListItem<TemplateResult> {
   return {
@@ -14,7 +15,11 @@ function item1({ buttonTemplate }: Templates): ActionListItem<TemplateResult> {
   };
 }
 
-function item2({ accordionTemplate, iconTemplate, richContentTemplate }: Templates): ActionListItem<TemplateResult> {
+function item2({
+  accordionTemplate,
+  contactInformationTemplate,
+  richContentTemplate,
+}: Templates): ActionListItem<TemplateResult> {
   return {
     title: "Neem contact op met het waterschap en de gemeente",
     flowLine: true,
@@ -40,15 +45,7 @@ function item2({ accordionTemplate, iconTemplate, richContentTemplate }: Templat
                   Houd de informatie bij de hand die te maken heeft met de werkzaamheden "Verharding aanbrengen of
                   vervangen".
                 </p>
-                <h5>Contactinfromatie Waterschap Amstel, Gooi en Vecht</h5>
-                <span>${iconTemplate({ icon: "call" })} Telefoon: 0900 9394</span>
-                <span
-                  >${iconTemplate({ icon: "air" })}
-                  <a href="#">www.agv.nl ${iconTemplate({ icon: "external-link" })}</a></span
-                >
-                <span
-                  >${iconTemplate({ icon: "buildings" })} Bezoekadres: Korte Ouderkerkerdijk 7, 1096AC Amsterdam</span
-                >
+                ${contactInformationTemplate(contactInformationContent())}
               `,
             }),
           },
