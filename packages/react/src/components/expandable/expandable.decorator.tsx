c757f3e8a9cc4fe1b8af5e3b00132083cf407b5e
Chris van der Leij <cvanderleij@ilionx.com>
2025-03-04 14:13:32 +0100

    #2987 [Task] Packages: Typescript updaten naar `~5.7.3` of hoger

--- packages/react/src/components/expandable/expandable.decorator.tsx
+++ packages/react/src/components/expandable/expandable.decorator.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { ExpandableDecorator } from "dso-toolkit";
 
-export const decorator: ExpandableDecorator<JSX.Element> = (story) => (
-  <>
-    <span>toggle open control in the controls panel to expand/collapse.</span>
-    {story()}
-    <style>
-      {`
-        dso-expandable[open]:not([open="false"]),
-        dso-expandable:not(.dso-hide):not([open="false"]) {
-          border: 1px solid #000;
-        }
-    `}
-    </style>
-  </>
-);
+export const decorator: ExpandableDecorator<JSX.Element> = (story) => {
+  const s = story();
+  if (!React.isValidElement(s)) {
+    throw new Error("Expected a valid JSX element");
+  }
+
+  return (
+    <>
+      <span>toggle open control in the controls panel to expand/collapse.</span>
+      {s}
+      <style>
+        {`
+            dso-expandable[open]:not([open="false"]),
+            dso-expandable:not(.dso-hide):not([open="false"]) {
+              border: 1px solid #000;
+            }
+        `}
+      </style>
+    </>
+  );
+};
