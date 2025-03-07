ba094445b3ef50d386299e325482db4ceaf0c3ec
Eric Tamminga <eric@iterox.com>
2025-03-07 12:53:28 +0100

    #2939 Voorbeeldpagina: Typografie en link

--- storybook/src/example-pages/Typografie.ts
+++ storybook/src/example-pages/Typografie.ts
@@ -1,10 +1,10 @@
 import { html } from "lit-html";
 
-import { examplePageFactory } from "../../example-page-factory";
+import { examplePageFactory } from "../example-page-factory";
 
 examplePageFactory(
   "Voorbeeldpagina's",
-  "Toepassingen/Componenten",
+  null,
   "Typografie",
   ({ anchorTemplate }) => html`
     <div class="container">
