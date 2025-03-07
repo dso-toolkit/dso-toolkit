04d4f54d29461ae760293107864406ebe14881f6
Eric Tamminga <eric@iterox.com>
2025-03-07 10:36:10 +0100

    #2939 Meerdere Highlight Boxes: Van patroon naar voorbeeldpagina

---
+++ storybook/src/example-pages/content/main-menu.content.ts
@@ -0,0 +1,23 @@
+export const mainMenu = [
+  {
+    label: "Home",
+    url: "#",
+    active: true,
+  },
+  {
+    label: "Vergunningscheck",
+    url: "#",
+  },
+  {
+    label: "Aanvragen",
+    url: "#",
+  },
+  {
+    label: "Regels op de kaart",
+    url: "#",
+  },
+  {
+    label: "Mijn Omgevingsloket",
+    url: "#",
+  },
+];
