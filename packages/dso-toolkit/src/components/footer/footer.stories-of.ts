5cf307e0047c5fa6b659bb2e271262a5da168180
Eric Tamminga <eric@iterox.com>
2025-03-07 09:16:56 +0100

    #2939 Footer: Van patroon naar HTML|CSS component

---
+++ packages/dso-toolkit/src/components/footer/footer.stories-of.ts
@@ -0,0 +1,44 @@
+import { ComponentAnnotations, Renderer } from "@storybook/types";
+
+import { Footer } from "./footer.models.js";
+
+import { StoriesParameters, StoryObj } from "../../template-container";
+import { compiler } from "markdown-to-jsx";
+import { MetaOptions } from "../../storybook/meta-options.interface";
+
+interface FooterStories {
+  Footer: StoryObj<Record<string, never>, Renderer>;
+}
+
+export interface FooterTemplates<TemplateFnReturnType> {
+  footerTemplate: (footer: Footer<TemplateFnReturnType>) => TemplateFnReturnType;
+  children: TemplateFnReturnType;
+}
+
+interface FooterStoriesParameters<Implementation, Templates, TemplateFnReturnType>
+  extends StoriesParameters<Implementation, Templates, TemplateFnReturnType, FooterTemplates<TemplateFnReturnType>> {}
+
+export function footerMeta<TRenderer extends Renderer>({ readme }: MetaOptions = {}): ComponentAnnotations<TRenderer> {
+  return {
+    parameters: {
+      docs: readme
+        ? {
+            page: () => compiler(readme),
+          }
+        : {},
+    },
+  };
+}
+
+export function footerStories<Implementation, Templates, TemplateFnReturnType>({
+  storyTemplates,
+  templateContainer,
+}: FooterStoriesParameters<Implementation, Templates, TemplateFnReturnType>): FooterStories {
+  return {
+    Footer: {
+      render: templateContainer.render(storyTemplates, (_args, { footerTemplate, children }) =>
+        footerTemplate({ children }),
+      ),
+    },
+  };
+}
--- storybook/src/example-pages/Patronen/footer.ts
+++
@@ -1,6 +0,0 @@
-import { html } from "lit-html";
-
-import { examplePageFactory } from "../../example-page-factory";
-import { footerPartial } from "../partials/footer";
-
-examplePageFactory("Patronen", "Footer", (templates) => html`<div class="container">${footerPartial(templates)}</div>`);
