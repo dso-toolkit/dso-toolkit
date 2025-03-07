1a05fd24576fca0ce6f356523675302f13846be5
Eric Tamminga <eric@iterox.com>
2025-03-07 12:07:00 +0100

    #2939 Snippets Table  Data Grid en Data Table verplaatst naar component Table

--- packages/react/src/components/table/table.content.tsx
+++ packages/react/src/components/table/table.content.tsx
@@ -119,3 +119,36 @@ export const sortedDescendingTable = {
     ["1", '<a href="#fabien">Fabien</a>', "Potencier", "fabpot", "<code>tr</code>", "eenhelelangekolomdienietbreekt"],
   ],
 };
+
+const actions =
+  '<button type="button" class="dso-tertiary"><span class="sr-only">Bewerk</span><dso-icon icon="pencil"></dso-icon></button><button type="button" class="dso-tertiary"><span class="sr-only">Verwijder</span><dso-icon icon="trash"></dso-icon></button>';
+
+export const dataGridTable = {
+  caption: "Overzicht toegevoegde documenten",
+  head: [
+    { label: "#" },
+    { label: "Documentnaam" },
+    { label: "Eigenaar" },
+    { label: "Upload datum" },
+    { label: "Acties" },
+  ],
+  rows: [
+    ["1", '<a href="#tekening">Tekening.jpg</a>', "P.K. Puk", "21-07-2019", actions],
+    ["2", '<a href="#omgevingsplan">Omgevingsplan.jpg</a>', "H.G. Griff", "22-07-2019", actions],
+    ["3", '<a href="#bodemonderzoek">Bodemonderzoek.jpg</a>', "P.K. Puk", "23-07-2019", actions],
+    ["4", '<a href="#maatregelen">Maatregelen.jpg</a>', "P.K. Puk", "23-07-2019", actions],
+    ["5", '<a href="#omgevingsplan">Plattegrond.jpg</a>', "P.K. Puk", "28-07-2019", actions],
+  ],
+};
+
+export const dataTableTable = {
+  caption: "Overzicht toegevoegde documenten",
+  head: [{ label: "#" }, { label: "Documentnaam" }, { label: "Eigenaar" }, { label: "Upload datum" }],
+  rows: [
+    ["1", '<a href="#tekening">Tekening.jpg</a>', "P.K. Puk", "21-07-2019"],
+    ["2", '<a href="#omgevingsplan">Omgevingsplan.jpg</a>', "H.G. Griff", "22-07-2019"],
+    ["3", '<a href="#bodemonderzoek">Bodemonderzoek.jpg</a>', "P.K. Puk", "23-07-2019"],
+    ["4", '<a href="#maatregelen">Maatregelen.jpg</a>', "P.K. Puk", "23-07-2019"],
+    ["5", '<a href="#omgevingsplan">Plattegrond.jpg</a>', "P.K. Puk", "28-07-2019"],
+  ],
+};
