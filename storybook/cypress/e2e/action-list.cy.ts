5be1a51ea7b33845726753c8c79a1da5bf7e63ff
Eric Tamminga <eric@iterox.com>
2025-03-27 16:52:14 +0100

    #2894 [Task] Action List: flaky screenshot test

--- storybook/cypress/e2e/action-list.cy.ts
+++ storybook/cypress/e2e/action-list.cy.ts
@@ -9,6 +9,14 @@ describe("ActionList", () => {
   });
 
   it("screenshot", () => {
+    cy.get("dso-action-list.hydrated")
+      .find("dso-action-list-item.hydrated dso-accordion-section.hydrated")
+      .should("have.class", "dso-accordion-neutral")
+      .get("dso-action-list.hydrated")
+      .find("dso-action-list-item.hydrated dso-accordion-section.hydrated")
+      .shadow()
+      .find(".dso-section-handle dso-icon.hydrated");
+
     cy.get("dso-action-list.hydrated").matchImageSnapshot();
   });
 });
