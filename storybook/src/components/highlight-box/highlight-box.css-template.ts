a67a57b183035d3da0461149b0629961067f19c0
Eric Tamminga <eric@iterox.com>
2025-04-10 09:08:31 +0200

    #3050 [Docs] Voorbeeldpagina: Highlight-box Banner aspect-ratio demo pagina missen iconen in buttons

--- storybook/src/components/highlight-box/highlight-box.css-template.ts
+++ storybook/src/components/highlight-box/highlight-box.css-template.ts
@@ -35,7 +35,7 @@ export const cssHighlightBox: ComponentImplementation<HighlightBox> = {
           ${stepCounter()}
           ${bannerImage
             ? imageTemplate({
-                source: "images/man-vrouw-kaart.png",
+                source: "images/banner-image.webp",
                 alt: "man en vrouw met kaart",
                 modifier: "dso-highlight-box-banner",
               })
