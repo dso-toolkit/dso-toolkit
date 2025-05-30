8e469696fd35eb89e25584b5f36256c80049a3c4
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2025-05-30 14:26:16 +0200

    #3032 Diverse test fixes

--- storybook/cypress/e2e/document-component.cy.ts
+++ storybook/cypress/e2e/document-component.cy.ts
@@ -41,33 +41,60 @@ describe("Document Component", () => {
 
   const states = ["default", "voegtoe", "verwijder"];
 
-  for (const wijzigactie of states) {
-    for (const annotationsWijzigactie of states) {
-      it(`matches image snapshot wijzigactie ${wijzigactie} with annotationsWijzigactie ${annotationsWijzigactie}`, () => {
+  for (const wijzigactieState of states) {
+    for (const annotationsWijzigactieState of states) {
+      it(`matches image snapshot wijzigactie ${wijzigactieState} with annotationsWijzigactie ${annotationsWijzigactieState}`, () => {
+        const wijzigactie = wijzigactieState === "default" ? null : wijzigactieState;
+        const annotationsWijzigactie = annotationsWijzigactieState === "default" ? null : annotationsWijzigactieState;
+
         // this test uses args to set the initial state of the component because the argsMapper is needed for the annotations
         cy.visit(
           "http://localhost:45000/iframe.html?id=core-document-component--default&args=open:!true;openAnnotation:!true",
         )
           .get("dso-document-component.hydrated")
-          .invoke("prop", "wijzigactie", wijzigactie === "default" ? null : wijzigactie)
-          .invoke(
-            "prop",
-            "annotationsWijzigactie",
-            annotationsWijzigactie === "default" ? null : annotationsWijzigactie,
-          )
-          .matchImageSnapshot();
+          .invoke("prop", "open", true)
+          .invoke("prop", "openAnnotation", true)
+          .invoke("prop", "wijzigactie", wijzigactie)
+          .invoke("prop", "annotationsWijzigactie", annotationsWijzigactie);
+
+        if (wijzigactie) {
+          cy.get("dso-document-component.hydrated").should("have.attr", "wijzigactie", wijzigactie);
+        } else {
+          cy.get("dso-document-component.hydrated").should("not.have.attr", "wijzigactie");
+        }
+
+        if (annotationsWijzigactie) {
+          cy.get("dso-document-component.hydrated").should(
+            "have.attr",
+            "annotations-wijzigactie",
+            annotationsWijzigactie,
+          );
+        } else {
+          cy.get("dso-document-component.hydrated").should("not.have.attr", "annotations-wijzigactie");
+        }
+
+        cy.get("dso-document-component.hydrated").matchImageSnapshot();
       });
     }
   }
 
   for (const state of ["default", "voegtoe", "verwijder"]) {
     it(`matches image snapshot ${state} - table-of-contents`, () => {
-      cy.visit(
-        "http://localhost:45000/iframe.html?id=core-document-component--default&args=filtered:!false;mode:table-of-contents",
-      )
+      const wijzigactie = state === "default" ? null : state;
+
+      cy.visit("http://localhost:45000/iframe.html?id=core-document-component--default")
         .get("dso-document-component.hydrated")
-        .invoke("prop", "wijzigactie", state === "default" ? null : state)
-        .matchImageSnapshot();
+        .invoke("prop", "wijzigactie", wijzigactie)
+        .invoke("prop", "filtered", false)
+        .invoke("prop", "mode", "table-of-contents");
+
+      if (wijzigactie) {
+        cy.get("dso-document-component.hydrated").should("have.attr", "wijzigactie", wijzigactie);
+      } else {
+        cy.get("dso-document-component.hydrated").should("not.have.attr", "wijzigactie");
+      }
+
+      cy.get("dso-document-component.hydrated").matchImageSnapshot();
     });
   }
 });
