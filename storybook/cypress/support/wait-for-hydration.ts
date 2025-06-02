27f9e72a9f7a5f1c8534aba70ebbbe81da1d701a
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2025-06-02 23:06:56 +0200

    #3032 Async issue tests voor Accordion en Table

--- storybook/cypress/support/wait-for-hydration.ts
+++ storybook/cypress/support/wait-for-hydration.ts
@@ -5,7 +5,26 @@ export function waitForHydration() {
     if (hasDsoComponents) {
       cy.get("*")
         .filter((_, el) => el.tagName.startsWith("DSO-"))
-        .should("have.class", "hydrated");
+        .should("have.class", "hydrated")
+        .then(($components) => {
+          $components.each((_, el) => {
+            if (el.tagName === "DSO-ACCORDION") {
+              let variant: string;
+
+              cy.wrap(el)
+                .invoke("prop", "variant")
+                .then((v) => {
+                  variant = v;
+                });
+
+              cy.wrap(el)
+                .find("dso-accordion-section")
+                .should((s) => expect(s).to.have.class(`dso-accordion-${variant}`));
+            } else if (el.tagName === "DSO-TABLE") {
+              cy.wrap(el).should("have.attr", "is-responsive");
+            }
+          });
+        });
     }
   });
 }
