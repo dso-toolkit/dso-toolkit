31c504803b9dad1b69b84f4ccb1ca7bd165192c6
Thomas Rijsewijk <tfrijsewijk@gmail.com>
2025-06-05 09:29:23 +0200

    #3032 Toelichting bij postbuild-transform.ts

--- storybook/scripts/postbuild-transform.ts
+++ storybook/scripts/postbuild-transform.ts
@@ -1,3 +1,18 @@
+/**
+ * Postbuild transform script
+ *
+ * Dit script draait na de build van het main Storybook project en vervangt alle <link> elementen met een data-dt-postbuild-href attribuut.
+ *
+ * Storybook biedt assets uit node_modules via twee verschillende manieren aan, afhankelijk van de modus:
+ *
+ * - storybook dev: assets worden ontsloten via `/static/packages`
+ * - storybook build: assets worden gebundeld in de build output en ontsloten via `/static/media`
+ *
+ * Als het project lokaal wordt opgestart, worden de `<link>` elementen zoals ze in `main.ts` zijn gedefinieerd, gebruikt. Als het project
+ * wordt gebouwd, worden deze `<link>` elementen aangepast naar de gebundelde versie in de build output. De gebundelde versie wordt
+ * gebruikt bij `yarn e2e` en op dso-toolkit.nl
+ */
+
 import { readFile, readdir, writeFile } from "fs/promises";
 import { basename, extname } from "path";
 
@@ -11,7 +26,7 @@ import { load } from "cheerio";
 
   const $ = load(buffer);
 
-  $("[data-dt-postbuild-href]").each((_, element) => {
+  $("link[href][data-dt-postbuild-href]").each((_, element) => {
     const $element = $(element);
     const href = $element.attr("href");
 
