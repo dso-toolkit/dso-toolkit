7209b2dd458c449bcd45c3a9bfd8698ac3385b95
Wesley Hendriks <wesleyhendriks@protonmail.ch>
2025-06-19 14:48:28 +0200

    #2487 [Added] Toggletip: Positioning strategy toegevoegd + linting

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -1248,6 +1248,10 @@ export namespace Components {
           * Set to true for small Toggletip.
          */
         "small"?: boolean;
+        /**
+          * Positioning strategy
+         */
+        "strategy": "absolute" | "fixed";
     }
     interface DsoTooltip {
         /**
@@ -3917,6 +3921,10 @@ declare namespace LocalJSX {
           * Set to true for small Toggletip.
          */
         "small"?: boolean;
+        /**
+          * Positioning strategy
+         */
+        "strategy"?: "absolute" | "fixed";
     }
     interface DsoTooltip {
         /**
