fa75fe06ebc4e7c4ea8ca15ea0dbc17be8ab3752
Eric Tamminga <eric@iterox.com>
2025-06-20 12:17:00 +0200

    #3157 Storybook 8 migratie

--- angular-workspace/.storybook/preview.ts
+++ angular-workspace/.storybook/preview.ts
@@ -1,24 +1,24 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import { Parameters } from "@storybook/types";
+import "@iframe-resizer/child";
 
 import * as docJson from "../documentation.json";
-
-import "@iframe-resizer/child";
 
 setCompodocJson(docJson);
 
 export const parameters: Parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-  docs: { inlineStories: true },
+  docs: { inlineStories: true, codePanel: true },
   options: {
     storySort: {
       method: "alphabetically",
     },
   },
 };
+
+export const tags = ["autodocs"];
