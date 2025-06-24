e233222dbfd81badb7ef533f05083dd0ec957501
Eric Tamminga <eric@iterox.com>
2025-06-24 08:08:15 +0200

    #2487 kleine aanpassingen aan backtick gebruik in comments

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -1280,15 +1280,15 @@ export namespace Components {
     }
     interface DsoToggletip {
         /**
-          * The badge status when `mode`=`badge`
+          * The badge status when `mode="badge"`
          */
         "badgeStatus"?: BadgeStatus;
         /**
-          * The icon when `mode`=`icon`
+          * The icon when `mode="icon"`
          */
         "icon"?: string;
         /**
-          * The icon when `mode`=`icon` and the tip is visible
+          * The icon when `mode="icon"` and the tip is visible
          */
         "iconActive"?: string;
         /**
@@ -4033,15 +4033,15 @@ declare namespace LocalJSX {
     }
     interface DsoToggletip {
         /**
-          * The badge status when `mode`=`badge`
+          * The badge status when `mode="badge"`
          */
         "badgeStatus"?: BadgeStatus;
         /**
-          * The icon when `mode`=`icon`
+          * The icon when `mode="icon"`
          */
         "icon"?: string;
         /**
-          * The icon when `mode`=`icon` and the tip is visible
+          * The icon when `mode="icon"` and the tip is visible
          */
         "iconActive"?: string;
         /**
