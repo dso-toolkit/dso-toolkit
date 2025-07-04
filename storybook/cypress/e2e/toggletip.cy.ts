b28b886d7090ff992cd33a6a78e504049ccbcef2
Eric Tamminga <eric@iterox.com>
2025-07-04 17:13:50 +0200

    #2487 e2e aangepast

--- storybook/cypress/e2e/toggletip.cy.ts
+++ storybook/cypress/e2e/toggletip.cy.ts
@@ -3,43 +3,22 @@ describe("Toggletip", () => {
     cy.visit("http://localhost:45000/iframe.html?id=core-toggletip--toggletip");
   });
 
-  it("should be accessible", () => {
-    cy.injectAxe();
-    cy.dsoCheckA11y("dso-toggletip.hydrated");
-  });
-
-  it("should focus on button after escape", () => {
-    cy.get("dso-toggletip.hydrated")
-      .as("dsoToggletip")
-      .shadow()
-      .find("dso-info-button")
-      .shadow()
-      .find("button")
-      .as("dsoButton")
-      .get("@dsoToggletip")
-      .shadow()
-      .find(".tooltip")
-      .as("dsoTooltip");
-
-    cy.get("@dsoButton")
-      .click()
-      .get("@dsoTooltip")
-      .should("not.have.class", "hidden")
-      .realPress("Tab")
-      .get("@dsoButton")
-      .should("not.have.focus")
-      .realPress("Escape")
-      .get("@dsoButton")
-      .should("have.focus");
-  });
-
   const modes = ["toggle", "secondary", "badge", "icon"];
 
   for (const mode of modes) {
-    it(`should show toggletip in ${mode} mode`, () => {
-      cy.visit(`http://localhost:45000/iframe.html?id=core-toggletip--toggletip&args=mode:${mode}`);
+    describe(`mode ${mode}`, () => {
+      beforeEach(() => {
+        cy.visit(`http://localhost:45000/iframe.html?id=core-toggletip--toggletip&args=mode:${mode}`);
+      });
 
-      cy.get("dso-toggletip.hydrated").should("be.visible").matchImageSnapshot();
+      it("should be accessible", () => {
+        cy.injectAxe();
+        cy.dsoCheckA11y("dso-toggletip.hydrated");
+      });
+
+      it(`should show toggletip`, () => {
+        cy.get("dso-toggletip.hydrated").should("be.visible").matchImageSnapshot();
+      });
     });
   }
 
