640679011b5b8b17bdc9296b5b906db7fb49c5ea
Eric Tamminga <eric@iterox.com>
2025-08-07 10:14:17 +0200

    #3157 Review Items Thomas 2

--- storybook/cypress/support/e2e.ts
+++ storybook/cypress/support/e2e.ts
@@ -21,6 +21,7 @@ import "cypress-real-events";
 
 import { addMatchImageSnapshotCommand } from "@simonsmith/cypress-image-snapshot/command";
 
+// Todo: In 3288 gaan we onderstaande afvang van "uncaught:exception" oplossen.
 Cypress.on("uncaught:exception", (_err, _runnable) => {
   // returning false here prevents Cypress from failing the test
   return false;
