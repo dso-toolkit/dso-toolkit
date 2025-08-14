9b2830bef0ed67da69698f6a3fda45ef46d1c93d
Eric Tamminga <eric@iterox.com>
2025-08-14 10:07:47 +0200

    #3157 gebruik preload links alleen in CI

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -47,32 +47,35 @@ const config: StorybookConfig = {
   },
   addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
   stories: ["../src/components/**/*.{core-,css-}stories.ts", "../src/example-pages/**/*.stories.ts"],
-  previewHead: (head) => `
-    ${head}
-    <link
-      rel="preload"
-      href="/assets/fonts/Asap/Asap-Italic-VariableFont_wdth,wght.ttf"
-      as="font"
-      type="font/ttf"
-      crossorigin
-      data-dt-postbuild-href
-    >
-    <link
-      rel="preload"
-      href="/assets/fonts/Asap/Asap-VariableFont_wdth,wght.ttf"
-      as="font"
-      type="font/ttf"
-      crossorigin
-      data-dt-postbuild-href
-    >
-    <link
-      rel="preload"
-      href="/assets/di.svg"
-      as="image"
-      type="image/svg+xml"
-      data-dt-postbuild-href
-    >
-  `,
+  previewHead: (head) =>
+    process.env.CI
+      ? `
+      ${head}
+      <link
+        rel="preload"
+        href="/assets/fonts/Asap/Asap-Italic-VariableFont_wdth,wght.ttf"
+        as="font"
+        type="font/ttf"
+        crossorigin
+        data-dt-postbuild-href
+      >
+      <link
+        rel="preload"
+        href="/assets/fonts/Asap/Asap-VariableFont_wdth,wght.ttf"
+        as="font"
+        type="font/ttf"
+        crossorigin
+        data-dt-postbuild-href
+      >
+      <link
+        rel="preload"
+        href="/assets/di.svg"
+        as="image"
+        type="image/svg+xml"
+        data-dt-postbuild-href
+      >
+    `
+      : head,
   // Onderstaande method is uitgezet in #2241, gaan we verder onderzoeken in #2302
   // previewBody: (body) =>
   //   !process.env.CI
