9f31a4cdb19acbe4b3f7f65d9ccecb15426d5320
cvanderleij-ilionx <cvanderleij@ilionx.com>
2025-08-18 09:07:12 +0200

    #3209 [Added] List: adds ordered-action modifier to list stories

--- packages/dso-toolkit/src/components/list/list.args.ts
+++ packages/dso-toolkit/src/components/list/list.args.ts
@@ -22,7 +22,7 @@ export const listArgTypes: ArgTypes<ListArgs> = {
     },
   },
   modifier: {
-    options: [undefined, "group", "columns", "img-list"],
+    options: [undefined, "group", "columns", "img-list", "ordered-action"],
     control: {
       type: "select",
     },
