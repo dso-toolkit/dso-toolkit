75aab01cb942bb1a70b76813e609a3432d10ff23
Chris van der Leij <cvanderleij@ilionx.com>
2025-10-07 16:36:06 +0200

    #3254 [Added] Document Component: `gereserveerd` en `vervallen` als XML

--- storybook/cypress/e2e/document-component.cy.ts
+++ storybook/cypress/e2e/document-component.cy.ts
@@ -1,17 +1,37 @@
 import { DocumentComponentMarkFunction } from "@dso-toolkit/core/src/components";
+import { DocumentComponent } from "dso-toolkit";
+import { TemplateResult } from "lit-html";
 
 import { isOdd } from "../support/is-odd";
 
+function setProps(props: Partial<DocumentComponent<TemplateResult>>) {
+  return cy.get("@document-component").then(($el) => {
+    Object.entries(props).forEach(([key, value]) => {
+      cy.wrap($el).invoke("prop", key, value);
+    });
+  });
+}
+
+function expectAlert(text: string | undefined = undefined) {
+  cy.get("@document-component")
+    .shadow()
+    .find("dso-alert.hydrated")
+    .should(text ? "have.text" : "not.exist", text);
+}
+
 describe("Document Component", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:45000/iframe.html?id=core-document-component--default");
+    cy.get("dso-document-component").as("document-component").should("have.class", "hydrated");
+  });
+
   it("should mark and highlight", () => {
     const marker: DocumentComponentMarkFunction = (text, source) =>
       text
         .split(new RegExp(`(k)`, "gi"))
         .map((item, index) => (isOdd(index) ? { text: item, highlight: source === "kop" && index === 1 } : item));
 
-    cy.visit("http://localhost:45000/iframe.html?id=core-document-component--default")
-      .get("dso-document-component")
-      .should("have.class", "hydrated")
+    cy.get("@document-component")
       .then(($documentComponent) => ($documentComponent[0].mark = cy.spy(marker).as("marker")))
       .get("@marker")
       .invoke("getCalls")
@@ -27,8 +47,7 @@ describe("Document Component", () => {
   });
 
   it("shows a badge with an exclamationmark with tooltip", () => {
-    cy.visit("http://localhost:45000/iframe.html?id=core-document-component--default")
-      .get("dso-document-component")
+    cy.get("@document-component")
       .invoke("attr", "geneste-ontwerp-informatie", true)
       .invoke("attr", "bevat-ontwerp-informatie", false)
       .shadow()
@@ -49,32 +68,25 @@ describe("Document Component", () => {
         const annotationsWijzigactie = annotationsWijzigactieState === "default" ? null : annotationsWijzigactieState;
 
         // this test uses args to set the initial state of the component because the argsMapper is needed for the annotations
-        cy.visit(
-          "http://localhost:45000/iframe.html?id=core-document-component--default&args=open:!true;openAnnotation:!true",
-        )
-          .get("dso-document-component.hydrated")
+        cy.get("@document-component")
           .invoke("prop", "open", true)
           .invoke("prop", "openAnnotation", true)
           .invoke("prop", "wijzigactie", wijzigactie)
           .invoke("prop", "annotationsWijzigactie", annotationsWijzigactie);
 
         if (wijzigactie) {
-          cy.get("dso-document-component.hydrated").should("have.attr", "wijzigactie", wijzigactie);
+          cy.get("@document-component").should("have.attr", "wijzigactie", wijzigactie);
         } else {
-          cy.get("dso-document-component.hydrated").should("not.have.attr", "wijzigactie");
+          cy.get("@document-component").should("not.have.attr", "wijzigactie");
         }
 
         if (annotationsWijzigactie) {
-          cy.get("dso-document-component.hydrated").should(
-            "have.attr",
-            "annotations-wijzigactie",
-            annotationsWijzigactie,
-          );
+          cy.get("@document-component").should("have.attr", "annotations-wijzigactie", annotationsWijzigactie);
         } else {
-          cy.get("dso-document-component.hydrated").should("not.have.attr", "annotations-wijzigactie");
+          cy.get("@document-component").should("not.have.attr", "annotations-wijzigactie");
         }
 
-        cy.get("dso-document-component.hydrated").matchImageSnapshot();
+        cy.get("@document-component").matchImageSnapshot();
       });
     }
   }
@@ -83,23 +95,85 @@ describe("Document Component", () => {
     it(`matches image snapshot ${state} - table-of-contents`, () => {
       const wijzigactie = state === "default" ? null : state;
 
-      cy.visit("http://localhost:45000/iframe.html?id=core-document-component--default")
-        .get("dso-document-component.hydrated")
+      cy.get("@document-component")
         .invoke("prop", "wijzigactie", wijzigactie)
         .invoke("prop", "filtered", false)
         .invoke("prop", "mode", "table-of-contents");
 
       if (wijzigactie) {
-        cy.get("dso-document-component.hydrated").should("have.attr", "wijzigactie", wijzigactie);
+        cy.get("@document-component").should("have.attr", "wijzigactie", wijzigactie);
       } else {
-        cy.get("dso-document-component.hydrated").should("not.have.attr", "wijzigactie");
+        cy.get("@document-component").should("not.have.attr", "wijzigactie");
       }
 
-      cy.get("dso-document-component.hydrated")
-        .should("have.attr", "mode", "table-of-contents")
-        .and("not.have.attr", "filtered");
+      cy.get("@document-component").should("have.attr", "mode", "table-of-contents").and("not.have.attr", "filtered");
 
-      cy.get("dso-document-component.hydrated").matchImageSnapshot();
+      cy.get("@document-component").matchImageSnapshot();
     });
   }
+
+  it("shows reserved alert when gereserveerd prop is set with XML", () => {
+    setProps({ open: true, gereserveerd: `<Gereserveerd wijzigactie=''></Gereserveerd>`, vervallen: "" });
+
+    cy.get("@document-component").shadow().find(".heading-element dso-label").should("have.text", "gereserveerd");
+
+    expectAlert("Dit onderdeel is gereserveerd voor toekomstige toevoeging.");
+  });
+
+  it("shows label with status verwijder when gereserveerd prop is set with verwijder XML", () => {
+    setProps({ open: true, gereserveerd: `<Gereserveerd wijzigactie='verwijder'></Gereserveerd>`, vervallen: "" });
+
+    cy.get("@document-component")
+      .shadow()
+      .find(".heading-element dso-label")
+      .should("have.text", "gereserveerd")
+      .should("have.attr", "status", "verwijderd");
+
+    expectAlert();
+  });
+
+  it("shows label with a verwijderd status and vervallen label with toegevoegd status when gereserveerd and vervallen props are set", () => {
+    setProps({
+      open: true,
+      gereserveerd: `<Gereserveerd wijzigactie='verwijder'></Gereserveerd>`,
+      vervallen: `<Vervallen wijzigactie='voegtoe'></Vervallen>`,
+    });
+
+    cy.get("@document-component")
+      .shadow()
+      .find(".heading-element")
+      .within(() => {
+        cy.get("dso-label[status='verwijderd']").should("exist");
+        cy.get("dso-label[status='toegevoegd']").should("exist");
+      });
+
+    cy.get("@document-component").shadow().find("dso-alert.hydrated").should("not.exist");
+  });
+
+  it("shows vervallen label with status toegevoegd when vervallen prop is set with XML", () => {
+    setProps({
+      open: true,
+      gereserveerd: "",
+      vervallen: `<Vervallen wijzigactie='voegtoe'></Vervallen>`,
+    });
+
+    cy.get("@document-component")
+      .shadow()
+      .find(".heading-element dso-label")
+      .should("have.attr", "status", "toegevoegd");
+
+    expectAlert();
+  });
+
+  it("shows vervallen label with when vervallen prop is set with XML", () => {
+    setProps({
+      open: true,
+      gereserveerd: "",
+      vervallen: `<Vervallen></Vervallen>`,
+    });
+
+    cy.get("@document-component").shadow().find(".heading-element dso-label").should("have.text", "vervallen");
+
+    expectAlert("Dit onderdeel is vervallen.");
+  });
 });
