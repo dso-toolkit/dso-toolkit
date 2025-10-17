830367879a4624cd5ec529fd2ac642be09052d48
Eric Tamminga <eric@iterox.com>
2025-10-17 16:17:51 +0200

    #3345 [Fixed] Viewer Grid: Filter Panel blijft in DOM aanwezig

--- packages/core/src/components/viewer-grid/components/filter-panel.tsx
+++ packages/core/src/components/viewer-grid/components/filter-panel.tsx
@@ -1,21 +1,31 @@
-import { FunctionalComponent, h } from "@stencil/core";
+import { Fragment, FunctionalComponent, h } from "@stencil/core";
 
 export interface ViewerGridFilterPanelProps {
+  open: boolean;
   title?: string;
   ref: ((element: HTMLDialogElement | undefined) => void) | undefined;
   dsoCloseFilterPanel: (event: MouseEvent | Event) => void;
 }
 
-export const FilterPanel: FunctionalComponent<ViewerGridFilterPanelProps> = ({ title, ref, dsoCloseFilterPanel }) => (
+export const FilterPanel: FunctionalComponent<ViewerGridFilterPanelProps> = ({
+  open,
+  title,
+  ref,
+  dsoCloseFilterPanel,
+}) => (
   <dialog class="filter-panel" ref={ref}>
-    {title && <h3>{title}</h3>}
-    <dso-icon-button
-      class="dso-close"
-      icon="times"
-      variant="tertiary"
-      label="Sluiten"
-      onDsoClick={dsoCloseFilterPanel}
-    />
-    <slot name="filter-panel" />
+    {open && (
+      <Fragment>
+        {title && <h3>{title}</h3>}
+        <dso-icon-button
+          class="dso-close"
+          icon="times"
+          variant="tertiary"
+          label="Sluiten"
+          onDsoClick={dsoCloseFilterPanel}
+        />
+        <slot name="filter-panel" />
+      </Fragment>
+    )}
   </dialog>
 );
