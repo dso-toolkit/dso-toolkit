b978d2298ea0820c4e02ede55c870b2060ad3ee9
Chris van der Leij <cvanderleij@ilionx.com>
2025-11-21 09:29:54 +0100

    #3306 [Task] Packages: rollback main.ts

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -46,11 +46,56 @@ const config: StorybookConfig = {
   },
   addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
   stories: ["../src/components/**/*.{core-,css-}stories.ts", "../src/example-pages/**/*.stories.ts"],
+  previewHead: (head) =>
+    process.env.CI
+      ? `
+      ${head}
+      <link
+        rel="preload"
+        href="/assets/fonts/Asap/Asap-Italic-VariableFont_wdth,wght.ttf"
+        as="font"
+        type="font/ttf"
+        crossorigin
+        data-dt-postbuild-href
+      >
+      <link
+        rel="preload"
+        href="/assets/fonts/Asap/Asap-VariableFont_wdth,wght.ttf"
+        as="font"
+        type="font/ttf"
+        crossorigin
+        data-dt-postbuild-href
+      >
+      <link
+        rel="preload"
+        href="/assets/di.svg"
+        as="image"
+        type="image/svg+xml"
+        data-dt-postbuild-href
+      >
+    `
+      : head,
+  // Onderstaande method is uitgezet in #2241, gaan we verder onderzoeken in #2302
+  // previewBody: (body) =>
+  //   !process.env.CI
+  //     ? `
+  //     ${body}
+  //     <iframe title="Stencil Dev Server Connector ⚡" src="/~dev-server" style="display:block;width:0;height:0;border:0;visibility:hidden" aria-hidden="true"></iframe>
+  //   `
+  //     : body,
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import("vite");
+
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+    });
+  },
   core: {
-    builder: "@storybook/builder-vite", // ✅ geen pad
+    builder: "@storybook/builder-vite",
     disableTelemetry: true,
   },
-  framework: "@storybook/web-components-vite", // ✅ geen pad
+  framework: "@storybook/web-components-vite",
 };
 
 export default config;
