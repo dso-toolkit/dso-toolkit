50e070b73a2b7dd7f10ac4c95d04bad678c5244d
Chris van der Leij <cvanderleij@ilionx.com>
2025-11-21 10:56:00 +0100

    #3306 [Task] Packages: remove isStoryFnAngularReturnTypeTemplate and set core, react and angular packages to type module

--- angular-workspace/components/expandable/expandable.stories.ts
+++ angular-workspace/components/expandable/expandable.stories.ts
@@ -3,7 +3,6 @@ import { ExpandableArgs, expandableMeta, expandableStories } from "dso-toolkit";
 
 import { DsoExpandable } from "../../projects/component-library/src/public-api";
 import { templateContainer } from "../../templates";
-import { isStoryFnAngularReturnTypeTemplate } from "../helpers";
 
 import { expandableContent } from "./expandable.content";
 import readme from "./readme.md?raw";
@@ -32,17 +31,10 @@ const { Default, WithAnimation } = expandableStories({
   },
   decorator: (story) => {
     const s = story();
-    if (!isStoryFnAngularReturnTypeTemplate(s)) {
-      throw new Error("Expected a valid Angular template");
-    }
-
-    const { props, template } = s;
-
     return {
-      props,
       template: `
       <span>toggle open control in the controls panel to expand/collapse.<span>
-      ${template}
+      ${s.template}
       <style>
         dso-expandable[open],
         dso-expandable:not(.dso-hide) {
