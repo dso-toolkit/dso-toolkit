98e527ac83ac87a29b43602712bc6c1e3e1d3a83
Eric Tamminga <eric@iterox.com>
2025-12-30 14:15:11 +0100

    #3461 [Changed] Ozon Content: voorkom markeren `figuur-titel` in Image Overlay

--- packages/core/src/components/ozon-content/ozon-content-node.interface.ts
+++ packages/core/src/components/ozon-content/ozon-content-node.interface.ts
@@ -9,5 +9,5 @@ export interface OzonContentNode<T = unknown> {
 
   identify?(node: Node): string | undefined;
 
-  render(node: Node, context: OzonContentNodeContext<T>): JSX.Element;
+  render(node: Node, context: OzonContentNodeContext<T>, ignoreMark?: boolean): JSX.Element;
 }
