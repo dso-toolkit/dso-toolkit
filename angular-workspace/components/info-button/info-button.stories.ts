8b3809724f8b7b92d0e8500b227ad89b03bc357c
Chris van der Leij <cvanderleij@ilionx.com>
2026-01-23 12:27:09 +0100

    #3395 UX feedback (rename stories and e2e)

--- angular-workspace/components/info-button/info-button.stories.ts
+++ angular-workspace/components/info-button/info-button.stories.ts
@@ -19,7 +19,7 @@ const meta: Meta<InfoButtonArgs> = {
 
 export default meta;
 
-const { Active, Inactive, SecondaryActive, SecondaryInactive, WithToggletip } = infoButtonStories({
+const { Default, Information, SecondaryActive, SecondaryInactive } = infoButtonStories({
   templateContainer,
   storyTemplates: (templates) => {
     const { infoButtonTemplate } = templates;
@@ -31,4 +31,4 @@ const { Active, Inactive, SecondaryActive, SecondaryInactive, WithToggletip } = 
   },
 });
 
-export { Active, Inactive, SecondaryActive, SecondaryInactive, WithToggletip };
+export { Default, Information, SecondaryActive, SecondaryInactive };
