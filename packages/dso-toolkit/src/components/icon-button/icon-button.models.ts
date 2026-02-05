9458aa92298c908152dfa86ac85a28ec13453b9c
Eric Tamminga <eric@iterox.com>
2026-02-05 09:27:22 +0100

    #2542 type IconAlias in models doorgevoerd

--- packages/dso-toolkit/src/components/icon-button/icon-button.models.ts
+++ packages/dso-toolkit/src/components/icon-button/icon-button.models.ts
@@ -1,12 +1,14 @@
 import { HandlerFunction } from "storybook/actions";
 
+import { IconAlias } from "../icon";
+
 export type IconButtonVariant = "secondary" | "tertiary" | "map";
 
 export type IconButtonTooltipPlacement = "top" | "bottom" | "left" | "right";
 
 export interface IconButton {
   variant: IconButtonVariant;
-  icon: string;
+  icon: IconAlias;
   label: string;
   tooltipPlacement?: IconButtonTooltipPlacement;
   disabled?: boolean;
