34079719dbd54ad5903b4c44e70902409cc20b1b
Wouter Heldens <wheldens@gmail.com>
2026-02-13 16:43:06 +0100

    fix lint & format

--- storybook/src/components/legend/legend.core-stories.ts
+++ storybook/src/components/legend/legend.core-stories.ts
@@ -1,12 +1,12 @@
-import legendReadme from "@dso-toolkit/core/src/components/legend/readme.md?raw";
 import legendGroupReadme from "@dso-toolkit/core/src/components/legend/legend-group/readme.md?raw";
 import legendItemReadme from "@dso-toolkit/core/src/components/legend/legend-item/readme.md?raw";
+import legendReadme from "@dso-toolkit/core/src/components/legend/readme.md?raw";
 import type { Meta } from "@storybook/web-components-vite";
 import { LegendArgs, legendMeta, legendStories } from "dso-toolkit";
 
 import { templateContainer } from "../../templates";
-import { decorator } from "./legend-item/legend-item.decorator";
 
+import { decorator } from "./legend-item/legend-item.decorator";
 import { kaartlagenRichContent, legendaRichContent } from "./legend.content";
 
 /** Strips sections that break markdown-to-jsx */
