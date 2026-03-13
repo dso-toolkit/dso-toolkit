344ada7c5eb20ea586d631e9922bca626d17d7ec
Wouter <wheldens@gmail.com>
2026-03-13 15:08:31 +0100

    #3447 [Added] Angular Output: ngModel toevoegen aan form web components

---
+++ angular-workspace/projects/component-library/src/lib/value-accessors/boolean-value-accessor.ts
@@ -0,0 +1,48 @@
+import { Directive, ElementRef, HostListener, forwardRef } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+@Directive({
+  selector: "dso-selectable[type=checkbox]",
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => BooleanValueAccessor),
+      multi: true,
+    },
+  ],
+})
+export class BooleanValueAccessor implements ControlValueAccessor {
+  private onChange = (_: boolean) => {};
+  private onTouched = () => {};
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener("focusout")
+  onFocusOut() {
+    this.onTouched();
+  }
+
+  @HostListener("dsoChange", ["$event"])
+  onDsoChange(event: CustomEvent) {
+    const checked = event.detail?.checked ?? false;
+    this.el.nativeElement.checked = checked;
+    this.onChange(checked);
+  }
+
+  writeValue(value: unknown) {
+    this.el.nativeElement.checked = value ?? false;
+  }
+
+  registerOnChange(fn: (value: boolean) => void) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void) {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.el.nativeElement.disabled = isDisabled;
+  }
+}
