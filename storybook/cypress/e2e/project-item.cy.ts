5750420f1062a2331786a863e194c2f6f7402bc5
xiangjuzou-prog <xiangju.zou@kadaster.nl>
2026-03-20 12:45:27 +0100

    #3607 Resolve comments

--- storybook/cypress/e2e/project-item.cy.ts
+++ storybook/cypress/e2e/project-item.cy.ts
@@ -1,31 +1,19 @@
-const selector = "dso-project-item.hydrated";
-const projectItemUrl = "http://localhost:45000/iframe.html?id=core-project-item--default";
-
 describe("Project Item", () => {
   beforeEach(() => {
-    cy.visit(projectItemUrl);
-    cy.get(selector).as("projectItem");
+    cy.visit("http://localhost:45000/iframe.html?id=core-project-item--default");
+    cy.get("dso-project-item.hydrated").as("projectItem");
   });
 
   it("should be accessible", () => {
     cy.injectAxe();
-    cy.dsoCheckA11y(selector);
+    cy.dsoCheckA11y("dso-project-item.hydrated");
   });
 
   it("matches snapshot", () => {
     cy.get("@projectItem").matchImageSnapshot("project-item");
   });
 
-  it("should render label prop", () => {
-    cy.get("@projectItem").invoke("attr", "label", "TestLabel");
-    cy.get("@projectItem")
-      .should("have.attr", "label", "TestLabel")
-      .shadow()
-      .find(".project-item-title dso-label")
-      .should("contain.text", "TestLabel");
-  });
-
-  it("should update label prop dynamically", () => {
+  it("should render and update label prop", () => {
     cy.get("@projectItem").invoke("attr", "label", "InitialLabel");
     cy.get("@projectItem")
       .should("have.attr", "label", "InitialLabel")
@@ -39,16 +27,5 @@ describe("Project Item", () => {
       .shadow()
       .find(".project-item-title dso-label")
       .should("contain.text", "UpdatedLabel");
-  });
-
-  it("should render slot content", () => {
-    cy.get("@projectItem").then(($el) => {
-      const span = document.createElement("span");
-      span.setAttribute("slot", "test-slot");
-      span.textContent = "Test slot content";
-      $el[0].appendChild(span);
-    });
-
-    cy.get("@projectItem").find('[slot="test-slot"]').should("exist").should("contain.text", "Test slot content");
   });
 });
