2e965aea07d2916ec799e3228a6ecf3cbad7fb02
xiangjuzou-prog <xiangju.zou@kadaster.nl>
2026-03-20 12:45:27 +0100

    #3607 Resolve comments

--- storybook/cypress/e2e/list-button.cy.ts
+++ storybook/cypress/e2e/list-button.cy.ts
@@ -1,196 +1,34 @@
-describe("ListButton", () => {
+const selector = "dso-project-item.hydrated";
+const projectItemUrl = "http://localhost:45000/iframe.html?id=core-project-item--default";
+
+describe("Project Item", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:45000/iframe.html?id=core-list-button--single-select");
-  });
-
-  it("should render label and sublabel correctly", () => {
-    cy.get("dso-list-button.hydrated")
-      .as("dsoListButton")
-      .get("@dsoListButton")
-      .should("be.visible")
-      .shadow()
-      .as("dsoListButtonShadow")
-      .find(".dso-selectable-input-wrapper > label")
-      .should("contain.text", "Milieubelastende activiteit - Melding")
-      .get("@dsoListButtonShadow")
-      .find(".dso-input-number")
-      .should("not.exist")
-      .get("@dsoListButtonShadow")
-      .find(".dso-sublabel")
-      .should("not.exist")
-      .get("@dsoListButton")
-      .invoke("attr", "sublabel", "Sublabel")
-      .get("@dsoListButtonShadow")
-      .find(".dso-sublabel")
-      .should("contain.text", "Sublabel")
-      .get("@dsoListButton")
-      .then(($listButton) => {
-        $listButton.append('<span slot="subcontent">Subcontent</span>');
-      })
-      .get("@dsoListButtonShadow")
-      .find(".dso-selectable-input-wrapper > label")
-      .should(
-        "contain.html",
-        'Milieubelastende activiteit - Melding<span class="sr-only"> Sublabel</span><span class="sr-only"> <span>Subcontent</span></span>',
-      )
-      .get("@dsoListButton")
-      .find('> [slot="subcontent"]')
-      .should("have.attr", "aria-hidden", "true")
-      .get("@dsoListButton")
-      .matchImageSnapshot();
-  });
-
-  it("should render subcontent in slot without prefix", () => {
-    cy.get("dso-list-button").invoke("append", `<span slot="subcontent">Subcontent met <strong>HTML</strong></span>`);
-
-    cy.get("dso-list-button.hydrated")
-      .shadow()
-      .find('slot[name="subcontent"]')
-      .invoke("get", 0)
-      .invoke("assignedNodes")
-      .then((assignedNodes) => {
-        cy.get('dso-list-button > [slot="subcontent"]').invoke("get", 0).should("equal", assignedNodes[0]);
-      });
-
-    cy.get("dso-list-button")
-      .shadow()
-      .as("dsoListButtonShadow")
-      .find(".dso-selectable-input-wrapper > label > span > .sr-only")
-      .should("contain.html", "<span>Subcontent met <strong>HTML</strong></span>")
-      .get("dso-list-button.hydrated")
-      .matchImageSnapshot();
-  });
-
-  it("should render subcontent in slot with prefix", () => {
-    cy.get("dso-list-button")
-      .invoke("append", `<span slot="subcontent">Subcontent met <strong>HTML</strong></span>`)
-      .get("dso-list-button")
-      .invoke("prop", "subcontentPrefix", "subcontentPrefix")
-      .shadow()
-      .find(".dso-selectable-input-wrapper > label")
-      .should(
-        "contain.html",
-        'Milieubelastende activiteit - Melding<span class="sr-only">subcontentPrefix: <span>Subcontent met <strong>HTML</strong></span></span>',
-      );
+    cy.visit(projectItemUrl);
+    cy.get(selector).as("projectItem");
   });
 
   it("should be accessible", () => {
     cy.injectAxe();
-    cy.dsoCheckA11y("dso-list-button.hydrated");
+    cy.dsoCheckA11y(selector);
   });
 
-  it("should render the interactive count input", () => {
-    cy.get("dso-list-button")
-      .as("dsoListButton")
-      .invoke("attr", "count", 8)
-      .shadow()
-      .as("dsoListButtonShadow")
-      .find(".dso-input-step-counter")
-      .should("be.visible")
-      .should("have.text", 8)
-      .get("@dsoListButtonShadow")
-      .find(".dso-input-number dso-icon-button")
-      .should("have.length", 2);
+  it("matches snapshot", () => {
+    cy.get("@projectItem").matchImageSnapshot("project-item");
   });
 
-  it("should hide the minus button when count is 1", () => {
-    cy.get("dso-list-button")
-      .as("dsoListButton")
-      .invoke("attr", "count", 1)
+  it("should render and update label prop", () => {
+    cy.get("@projectItem").invoke("attr", "label", "InitialLabel");
+    cy.get("@projectItem")
+      .should("have.attr", "label", "InitialLabel")
       .shadow()
-      .as("dsoListButtonShadow")
-      .get("@dsoListButtonShadow")
-      .find(".dso-input-number dso-icon-button")
-      .should("have.length", 1);
-  });
+      .find(".project-item-title dso-label")
+      .should("contain.text", "InitialLabel");
 
-  it("should emit dsoCountChange", () => {
-    cy.get("dso-list-button.hydrated")
-      .as("dsoListButton")
-      .invoke("attr", "count", 8)
-      .get("dso-list-button")
-      .then(($listButton) => {
-        $listButton.on("dsoCountChange", cy.stub().as("dsoCountChangeListener"));
-      })
+    cy.get("@projectItem").invoke("attr", "label", "UpdatedLabel");
+    cy.get("@projectItem")
+      .should("have.attr", "label", "UpdatedLabel")
       .shadow()
-      .as("dsoListButtonShadow")
-      .find("dso-icon-button")
-      .first()
-      .click()
-      .get("@dsoCountChangeListener")
-      .invoke("getCalls")
-      .invoke("at", -1)
-      .its("args.0.detail.count")
-      .should("equal", 7)
-      .get("@dsoListButtonShadow")
-      .find("dso-icon-button")
-      .last()
-      .click()
-      .get("@dsoCountChangeListener")
-      .invoke("getCalls")
-      .invoke("at", -1)
-      .its("args.0.detail.count")
-      .should("equal", 8);
-  });
-
-  it("should only allow emit changes between min and max", () => {
-    cy.get("dso-list-button")
-      .as("dsoListButton")
-      .invoke("attr", "count", 2)
-      .invoke("attr", "min", 1)
-      .invoke("attr", "max", 3)
-      .then(($listButton) => {
-        $listButton.on("dsoCountChange", cy.stub().as("dsoCountChangeListener"));
-      })
-      .shadow()
-      .as("dsoListButtonShadow")
-      .find("dso-icon-button")
-      .first()
-      .click()
-      .get("@dsoCountChangeListener")
-      .should("have.been.calledOnce")
-      .get("@dsoListButtonShadow")
-      .find("dso-icon-button")
-      .last()
-      .click()
-      .click()
-      .shadow()
-      .find("button")
-      .should("be.disabled");
-  });
-
-  it("should emit dsoSelectedChange", () => {
-    cy.get("dso-list-button")
-      .as("dsoListButton")
-      .invoke("attr", "checked", "")
-      .get("@dsoListButton")
-      .then(($listButton) => {
-        $listButton.on("dsoSelectedChange", cy.stub().as("dsoSelectedChangeListener"));
-      })
-      .shadow()
-      .as("dsoListButtonShadow")
-      .find(".dso-list-button.dso-selected")
-      .should("be.visible")
-      .get("@dsoListButtonShadow")
-      .find(".dso-list-button")
-      .realClick()
-      .get("@dsoSelectedChangeListener")
-      .should("have.been.calledOnce")
-      .invoke("getCalls")
-      .invoke("at", -1)
-      .its("args.0.detail.checked")
-      .should("equal", false)
-      .get("@dsoListButtonShadow")
-      .find(".dso-list-button.dso-selected")
-      .should("not.exist");
-  });
-
-  it("should disable manual input when manual is false", () => {
-    cy.get("dso-list-button")
-      .invoke("attr", "count", 5)
-      .invoke("attr", "manual", "false")
-      .shadow()
-      .find(".dso-manual-input-button")
-      .should("not.exist");
+      .find(".project-item-title dso-label")
+      .should("contain.text", "UpdatedLabel");
   });
 });
