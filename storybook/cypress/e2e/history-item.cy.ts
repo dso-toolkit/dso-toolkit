537e659bfdf2e7841499f96b6ad62e1172621ee6
xiangjuzou-prog <xiangju.zou@kadaster.nl>
2026-03-26 15:48:33 +0100

    #3423 Reslove comments- Chris

--- storybook/cypress/e2e/history-item.cy.ts
+++ storybook/cypress/e2e/history-item.cy.ts
@@ -1,6 +1,6 @@
 describe("History Item", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:45000/iframe.html?id=core-history-item--in-werking");
+    cy.visit("http://localhost:45000/iframe.html?id=core-history-item--default");
   });
 
   it("should be accessible", () => {
