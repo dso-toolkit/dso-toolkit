81d522d4c6c85945ce24fb41d91998d4f71a3163
Wouter Heldens <wheldens@gmail.com>
2026-06-29 12:15:13 +0200

    #3778 [Task] build pnpm catalogs and strict peer dependencies

--- storybook/src/components/button-group/button-group.core-stories.ts
+++ storybook/src/components/button-group/button-group.core-stories.ts
@@ -1,5 +1,5 @@
 import readme from "@dso-toolkit/core/src/components/button-group/readme.md?raw";
-import type { Meta } from "@storybook/web-components";
+import type { Meta } from "@storybook/web-components-vite";
 import { ButtonGroupArgs, buttonGroupMeta, buttonGroupStories } from "dso-toolkit";
 
 import { templateContainer } from "../../templates";
