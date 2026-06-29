b3d01191b2814d739b3fc857b3c4cbef99c8b469
Eric Tamminga <eric@iterox.com>
2026-06-29 14:10:03 +0200

    #3677 Correctie e2e Row Equal Heights

--- storybook/cypress/e2e/row-equal-heights.cy.ts
+++ storybook/cypress/e2e/row-equal-heights.cy.ts
@@ -5,13 +5,16 @@ describe("Row Equal Heights", () => {
 
   it("Gives each box the same height", () => {
     cy.viewport(1320, 720);
-    cy.get(".dso-equal-heights :first-child dso-highlight-box").invoke("height").as("height_of_box");
 
-    cy.get(".dso-equal-heights dso-highlight-box").invoke("height").should("have.be", this.height_of_box);
+    cy.get(".dso-equal-heights :first-child dso-highlight-box")
+      .invoke("height")
+      .then((first) => {
+        cy.get(".dso-equal-heights dso-highlight-box").invoke("height").should("equal", first);
+      });
   });
 
   it("Gives each box 100% width in mobile viewport", () => {
     cy.viewport(320, 720);
-    cy.get("dso-highlight-box").invoke("width").should("be.equal", 209);
+    cy.get("dso-highlight-box").invoke("width").should("equal", 209);
   });
 });
