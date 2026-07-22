7d7d2249b1b0e255f77a64c9a2470201e300e1e3
Eric Tamminga <eric@iterox.com>
2026-07-22 15:13:33 +0200

    #3432 Placeholder story

--- packages/dso-toolkit/src/components/advanced-select/advanced-select.content.ts
+++ packages/dso-toolkit/src/components/advanced-select/advanced-select.content.ts
@@ -59,3 +59,49 @@ export const options: (AdvancedSelectOption<unknown> | AdvancedSelectGroup<unkno
     ],
   },
 ];
+
+export const placeholderOptions: (
+  AdvancedSelectOption<unknown> | AdvancedSelectGroup<unknown> | AdvancedSelectPlaceholder
+)[] = [
+  {
+    label: "Geldende versie",
+    variant: "success",
+    redirect: {
+      href: "#",
+      label: "Bekijk volledig wijzigingenoverzicht",
+    },
+    options: [
+      {
+        label: "In werking (laatst gewijzigd: 01-01-2024)",
+      },
+    ],
+  },
+  {
+    label: "Wijzigingsbesluiten",
+    activeLabel: "Besluit",
+    badgeLabel: "Toon toelichting op wijzigingsbesluiten",
+    variant: "primary",
+    toggletip: "Er zijn wijzigingsbesluiten genomen. Bekijk een besluit om de veranderingen te zien.",
+    redirect: {
+      href: "#",
+      label: "Bekijk historie",
+    },
+    summaryCounter: true,
+    options: [
+      {
+        label: "Citeertitel van het besluit (In werking per 01-03-2024)",
+      },
+      {
+        label: "Citeertitel van het besluit (In werking per 01-04-2024)",
+      },
+    ],
+  },
+  {
+    label: "Ontwerpbesluiten ter inzage",
+    placeholder: "Er zijn alleen ontwerpen met een afgeronde inzage termijn voor dit document.",
+    redirect: {
+      href: "#",
+      label: "Bekijk ontwerpen met afgeronde inzage termijn",
+    },
+  },
+];
