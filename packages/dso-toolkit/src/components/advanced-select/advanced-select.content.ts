a4f3d380d77292d73bb83229de8af492e7ec89fe
Eric Tamminga <eric@iterox.com>
2026-07-28 10:43:42 +0200

    #3432 [Changed] Advanced Select: Visuele aanpassingen

--- packages/dso-toolkit/src/components/advanced-select/advanced-select.content.ts
+++ packages/dso-toolkit/src/components/advanced-select/advanced-select.content.ts
@@ -1,12 +1,12 @@
-import { AdvancedSelectOptionOrGroup } from "./advanced-select.models.js";
+import { AdvancedSelectGroup, AdvancedSelectOption, AdvancedSelectPlaceholder } from "./advanced-select.models.js";
 
-export const options: AdvancedSelectOptionOrGroup<unknown>[] = [
+export const options: (AdvancedSelectOption<unknown> | AdvancedSelectGroup<unknown> | AdvancedSelectPlaceholder)[] = [
   {
     label: "Geldende versie",
     variant: "success",
     redirect: {
       href: "#",
-      label: "Bekijk eerder vastgestelde versies",
+      label: "Bekijk volledig wijzigingenoverzicht",
     },
     options: [
       {
@@ -15,7 +15,27 @@ export const options: AdvancedSelectOptionOrGroup<unknown>[] = [
     ],
   },
   {
-    label: "Ontwerp versies ter inzage",
+    label: "Wijzigingsbesluiten",
+    activeLabel: "Besluit",
+    badgeLabel: "Toon toelichting op wijzigingsbesluiten",
+    variant: "primary",
+    toggletip: "Er zijn wijzigingsbesluiten genomen. Bekijk een besluit om de veranderingen te zien.",
+    redirect: {
+      href: "#",
+      label: "Bekijk historie",
+    },
+    summaryCounter: true,
+    options: [
+      {
+        label: "Citeertitel van het besluit (In werking per 01-03-2024)",
+      },
+      {
+        label: "Citeertitel van het besluit (In werking per 01-04-2024)",
+      },
+    ],
+  },
+  {
+    label: "Ontwerpbesluiten ter inzage",
     activeLabel: "Ontwerp",
     variant: "warning",
     toggletip:
@@ -39,22 +59,7 @@ export const options: AdvancedSelectOptionOrGroup<unknown>[] = [
     ],
   },
   {
-    label: "Toekomstige besluitversies",
-    activeLabel: "Besluit",
-    variant: "primary",
-    toggletip: "Er zijn wijzigingsbesluiten genomen. Bekijk een besluit om de veranderingen te zien.",
-    summaryCounter: true,
-    options: [
-      {
-        label: "Citeertitel van het besluit (In werking per 01-03-2024)",
-      },
-      {
-        label: "Citeertitel van het besluit (In werking per 01-04-2024)",
-      },
-    ],
-  },
-  {
-    label: "Groep met placeholder",
+    label: "Ontwerpbesluiten ter inzage",
     placeholder: "Er zijn alleen ontwerpen met een afgeronde inzage termijn voor dit document.",
     redirect: {
       href: "#",
