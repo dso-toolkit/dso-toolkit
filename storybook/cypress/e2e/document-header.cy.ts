b4345ed61c636fb03648c8231dd24c105b21b9da
Eric Tamminga <eric@iterox.com>
2026-08-05 15:35:56 +0200

    #3899 [Changed] Document Header: WCAG bevindingen

---
+++ storybook/cypress/e2e/document-header.cy.ts
@@ -0,0 +1,34 @@
+describe("Document Header", () => {
+  beforeEach(() => {
+    cy.viewport(1280, 600);
+  });
+
+  const stickyStories = ["sticky", "sticky-besluitversie", "sticky-ontwerp"];
+  const stories = ["default", "default-besluitversie", "default-ontwerp", ...stickyStories];
+
+  for (const story of stories) {
+    it(`should be accessible (${story})`, () => {
+      cy.visit(`http://localhost:45000/iframe.html?id=html-css-document-header--${story}`);
+      cy.injectAxe();
+      cy.dsoCheckA11y(".dso-document-header");
+    });
+
+    it(`matches imageSnapshot (${story})`, () => {
+      cy.visit(`http://localhost:45000/iframe.html?id=html-css-document-header--${story}`);
+      cy.get(".dso-document-header").matchImageSnapshot(`${Cypress.currentTest.titlePath.join(" -- ")}`);
+    });
+  }
+
+  describe("Below 850px", () => {
+    beforeEach(() => {
+      cy.viewport(849, 600);
+    });
+
+    for (const story of stickyStories) {
+      it(`matches imageSnapshot (${story})`, () => {
+        cy.visit(`http://localhost:45000/iframe.html?id=html-css-document-header--${story}`);
+        cy.get(".dso-document-header").matchImageSnapshot(`${Cypress.currentTest.titlePath.join(" -- ")}`);
+      });
+    }
+  });
+});
